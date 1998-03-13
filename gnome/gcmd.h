480616917c654ceb3420989e6e116207b4b1665d
Miguel de Icaza <miguel@novell.com>
1998-03-13 05:20:55 +0000

    Missing file

---
+++ gnome/gcmd.h
@@ -0,0 +1,10 @@
+#ifndef __GCMD_H
+#define __GCMD_H
+
+void gnome_listing_cmd (GtkWidget *widget, WPanel *panel);
+void gnome_compare_panels (void);
+void gnome_open_terminal (void);
+void gnome_about_cmd (void);
+void gnome_quit_cmd (void);
+
+#endif /* __GCMD_H */
