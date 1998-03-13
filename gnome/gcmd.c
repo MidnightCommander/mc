bf937d037737c391df04659067d0523a1405e58d
Miguel de Icaza <miguel@novell.com>
1998-03-13 17:28:06 +0000

    Duh, I missed gcmd.c -miguel

---
+++ gnome/gcmd.c
@@ -0,0 +1,69 @@
+#include <config.h>
+#include "x.h"
+#include <stdio.h>
+#include <sys/stat.h>
+#include "dir.h"
+#include "panel.h"
+#include "gscreen.h"
+#include "main.h"
+#include "cmd.h"
+#include "boxes.h"
+#include "panelize.h"
+#include "gcmd.h"
+
+void
+gnome_listing_cmd (GtkWidget *widget, WPanel *panel)
+{
+	GtkAllocation *alloc = &GTK_WIDGET (panel->list)->allocation;
+	int   view_type, use_msformat;
+	char  *user, *status;
+	
+	view_type = display_box (panel, &user, &status, &use_msformat, get_current_index ());
+	
+	if (view_type == -1)
+		return;
+
+	configure_panel_listing (panel, view_type, use_msformat, user, status);
+}
+
+void
+gnome_compare_panels (void)
+{
+	if (get_other_panel () == NULL){
+		message (1, " Error ", " There is no other panel to compare contents to ");
+		return;
+	}
+	compare_dirs_cmd ();
+}
+
+void
+gnome_open_terminal (void)
+{
+	my_system (1, shell, "nxterm");
+}
+
+void
+gnome_about_cmd (void)
+{
+        GtkWidget *about;
+        gchar *authors[] = {
+		"The Midnight Commander Team",
+		"http://mc.blackdown.org/mc",
+		"bug reports: mc-bugs@nuclecu.unam.mx",
+		NULL
+	};
+
+        about = gnome_about_new (_("GNU Midnight Commander"), VERSION,
+				 "(C) 1994-1998 the Free Software Fundation",
+				 authors,
+				 _("The GNOME edition of the Midnight Commander file manager."),
+				 NULL);
+        gtk_widget_show (about);
+}
+
+void
+gnome_quit_cmd (void)
+{
+	gtk_main_quit ();
+}
+
