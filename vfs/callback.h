e6e9f038ed188051761c0659b647643e175c5473
Pavel Machek <pavel@suse.cz>
1998-06-08 13:00:29 +0000

    Fixed number of callbacks, added prototype of function to set callback. Nothing important.

--- vfs/callback.h
+++ vfs/callback.h
@@ -1,9 +1,17 @@
-/*
- * Warning: there's better version of this file somewhere else.
+#ifndef CALLBACK_H
+#define CALLBACK_H
+
+/* 
+ * All callbacks are char *func( char *msg );
+ * INFO/BOX should always return NULL;
  */
 
 #define CALL_INFO 0
 #define CALL_BOX 1
 #define CALL_PASSWD 2
 
-#define NUM_CALLBACKS 4
+#define NUM_CALLBACKS 3
+
+extern void vfs_set_callback( int num, void *func );
+
+#endif 
