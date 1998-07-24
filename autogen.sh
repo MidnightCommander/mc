0f93ec20cdedff43cead98b8754f16454a8ef203
Tom Tromey <tromey@cygnus.com>
1998-07-24 06:39:59 +0000

    Thu Jul 23 22:44:16 1998  Tom Tromey  <tromey@cygnus.com>
    	* Makefile.in (aclocal.m4): Depend on all source files.  Include
    	gnome-vfs.m4 and gnome-undelfs.m4.
    	(GNOME_M4_FILES): New macro.
    	* autogen.sh: Include gnome-vfs.m4 and gnome-undelfs.m4 in
    	aclocal.m4.
    	* configure.in: Use GNOME_VFS_CHECKS and GNOME_UNDELFS_CHECKS
    	macros instead of in-line checks.  Create vfs/Makefile from
    	vfs/Make-mc.in.

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,7 @@ test -z "$srcdir" && srcdir=.
 
 (
 cd $srcdir
-cat macros/gnome.m4 mc-aclocal.m4 gettext.m4 > aclocal.m4
+cat macros/gnome.m4 macros/gnome-vfs.m4 macros/gnome-undelfs.m4 mc-aclocal.m4 gettext.m4 > aclocal.m4
 autoheader
 autoconf
 )
