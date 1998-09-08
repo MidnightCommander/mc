340fe608011b60836775120aedad72d7bc24d1cd
Pavel Roskin <proski@gnu.org>
1998-09-08 12:21:59 +0000

    aclocal.m4 should not be deleted by autogen.sh if "macros" directory is missing

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,12 @@ test -z "$srcdir" && srcdir=.
 
 (
 cd $srcdir
-cat macros/gnome.m4 macros/gnome-vfs.m4 macros/gnome-undelfs.m4 macros/linger.m4 mc-aclocal.m4 gettext.m4 > aclocal.m4
+if test -d macros; then
+	cat macros/gnome.m4 macros/gnome-vfs.m4 macros/gnome-undelfs.m4 \
+	macros/linger.m4 mc-aclocal.m4 gettext.m4 > aclocal.m4
+else
+	echo macros directory not found, skipping generation of aclocal.m4
+fi
 autoheader
 autoconf
 )
