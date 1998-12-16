ba93bd26d48fe3b910fec53aec5a74f590ccb97a
Miguel de Icaza <miguel@novell.com>
1998-12-16 03:47:34 +0000

    Just for kicks

--- autogen.sh
+++ autogen.sh
@@ -8,7 +8,7 @@ test -z "$srcdir" && srcdir=.
 cd $srcdir
 if test -d macros; then
 	test -f mc-aclocal.m4 && test -f gettext.m4 && cat mc-aclocal.m4 gettext.m4 > acinclude.m4
-	aclocal -I macros $ACLOCAL_FLAGS
+	aclocal -I macros
 else
 	echo macros directory not found, skipping generation of aclocal.m4
 fi
