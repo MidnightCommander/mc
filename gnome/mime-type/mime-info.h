e0973b695120eec1cd4349262df41e53e6a33132
Jonathan Blandford <jrb@redhat.com>
1999-03-15 18:05:33 +0000

    Oh fooey.  I forgot a file again... )-: I should know better by now...
    1999-03-15  Jonathan Blandford  <jrb@redhat.com>
    
    	* capplets/mime-type/mime-info.c (write_initial_keys): Now we
    	explicitly remove then recreate the file, so that we can guarantee
    	that the mime-type gets reread.

---
+++ gnome/mime-type/mime-info.h
@@ -0,0 +1,17 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*- */
+/* Copyright (C) 1998 Redhat Software Inc. 
+ * Authors: Jonathan Blandford <jrb@redhat.com>
+ */
+#ifndef _MIME_INFO_H_
+#define _MIME_INFO_H_
+#include "gnome.h"
+#include <regex.h>
+/* Typedefs */
+void init_mime_info ();
+void discard_mime_info ();
+void set_mime_key_value (gchar *mime_type, gchar *key, gchar *value);
+const char * local_mime_get_value (const char *mime_type, char *key);
+void write_user_keys ();
+void write_initial_keys ();
+
+#endif
