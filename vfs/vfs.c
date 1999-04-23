bd8f6266b7b139251e0eb87e713e4cc5dc5a04d2
Pavel Machek <pavel@suse.cz>
1999-04-23 10:38:13 +0000

    Fix for solaris using 'l' instead of 'S'.

--- vfs/vfs.c
+++ vfs/vfs.c
@@ -1443,6 +1443,7 @@ int vfs_parse_filemode (char *p)
     switch (*(p++)){
 	case 'x': res |= 0010; break;
 	case 's': res |= 0010 | S_ISGID; break;
+        case 'l': /* Solaris produces these */
 	case 'S': res |= S_ISGID; break;
 	case '-': break;
 	default: return -1;
