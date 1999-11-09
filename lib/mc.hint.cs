06ff41e71bc7f743325c82d2a4e730dc8e755caa
Pavel Machek <pavel@suse.cz>
1999-11-09 10:05:06 +0000

    Added mc.hint.cs, czech file with hints.

---
+++ lib/mc.hint.cs
@@ -0,0 +1,47 @@
+Midnight Commander (c) 1995-1999, Free Software Foundation
+Tip: C-x t vlo¾í na pøíkazovou øádku oznaèené soubory.
+Tip: C-x p vlo¾í na pøíkazovou øádku jméno aktuálního adresáøe.
+Tip: K doplòování pou¾ijte M-Tab (nebo Esc+Tab). Dvojitým zadání získáte seznam.
+Tip: Pou¾itím M-p nebo M-n se pohybujete v historii pøíkazù.
+Tip: Chcete zadat speciální znak(napø. tabelátor)? Pou¾ijte Ctrl-Q a daný znak.
+Tip: Nechcete ji¾ vidìt tipy? Vypnìte je v Nastavení/Zobrazení/Dal¹í nastavení.
+Tip: Adresáøe vyberete pøidáním lomítka na konec vzorku.
+Tip: Pokud Vá¹ terminál nezní nebo nemá funkèní klávesy, pou¾ijte Esc+èíslo.
+Tip: Domácí stránka programu Midnight Commander je na http://www.gnome.org/mc/
+Tip: Poslední verze programu Midnight Commander je na ftp://ftp.gnome.org/pub/GNOME/sources/mc
+Tip: Zprávy o chybách posílejte na mc-bugs@nuclecu.unam.mx (anglicky).
+Tip: Pomocí klávesy Tab zmìníte aktuální panel.
+Tip: Stiskem Enter na souboru .tar nebo .tar.gz si prohlédnete obsah.
+Tip: Existuje také manuálová stránka [mc(1)].
+Tip: Pøejete si klávesy stejnì jako v lynxu? Nastavte to v Nastavení.
+Tip: Makra uvozená % pracují na pøíkazové øádce.
+Tip: Pomocí M-! mù¾ete spustit pøíkaz a prohlédnout si výstup v oknì.
+Tip: Zobrazení v panelech mù¾e být upraveno. Z 'man mc' se dozvíte více.
+Tip: %D/%T se rozvine na soubory oznaèené v druhém panelu.
+Tip: Chcete opìt vá¹ shell? Stisknìte C-o. Do MC se vrátíte opìt pøes C-o.
+Tip: Nastavením promìnné CDPATH mù¾ete ¹etøit klávesnici.
+Tip: Pokud chcete vidìt soubory zaèínající teèkou, øeknìte to v Nastavení.
+Tip: Chcete vidìt zálohy svých souborù (konèí ~). Pou¾ijte Nastavení.
+Tip: Máte opravdu mnoho pamìti? Zkuste nastavit limit ve VFS na -1.
+Tip: Doplòování funguje ve vstupních øádkách v¹ech dialogù. Pou¾ijte M-Tab.
+Tip: Na terminálech s pomalou odezvou skuste na parametr -s (mc -s).
+Tip: S nalezenými soubory mù¾ete pracovat. Stiknìte tlaèítko Panelizace.
+Tip: Pro opravdu slo¾ité operace zkuste pøíkaz Pøíkaz/Externí panelizace.
+Tip: Pokud chcete zmìnit adresáø pøi práci na pøíkoazové øádce, zkuste M-c.
+Poznámka: Pøíkazy shellu nefungují, pokud nepracujete na lokální systému souborù.
+Tip: Smazaný text obnovíte C-y.
+Tip: Pokud nìkteré klávesy nefunfují, pou¾ijte menu Nastavení/uèení kláves.
+Tip: F13 (nebo Shift-F3) vyvolá prohlí¾ení bez zpracování obsahu souboru.
+Tip: Pokud v xtermu nepracují klávesy, zkuste soubory xterm.ad, .ti a .tcap.
+Tip: Editor pro klávesu F4 urèíte pomocí promìnné prostøedí EDITOR.
+Tip: Externí prohlí¾eè urèíte promìnnou PAGER.
+Tip: V¹echny ¾ádosti o potvrzení mù¾ete zakázat v Nastavení/Potvrzování.
+Tip: K seznam èasto pou¾ívaných adresáøù se mù¾ete dostat i pøes C-\.
+Tip: Na anonymní FTP se pøipojíte pomocí 'cd /#ftp:stroj.domena.cz'
+Tip: MC má vestavìný FTP protokol. Podívejte se do menu Levý/Ftp spojení.
+Tip: M-t rychle mìní formát zobrazení panelù.
+Tip: Pøi FTP spojení mù¾ete zadat i jméno: 'cd /#ftp:jmeno@stroj.domena.cz'
+Tip: RPM soubory si prohlédnete stiskem Enter.
+Tip: Pokud chcete oznaèit adresáø, pøidejte '/' (lomítko).
+Tip: Chcete-li pou¾ít my¹ pro zkopírování nebo vlo¾ení textu, stisknìte Shift.
+Tip: V seznamu adresáøù mù¾ete mít i èasto nav¹tìvované ftp servery. Zkuste C-\.
