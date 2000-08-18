556b80503ce144ccc33371c5ce8b4b19491fb4a8
Pavel Roskin <proski@gnu.org>
2000-08-18 04:16:25 +0000

    * configure.in: Simplified dependency tracking by using "-include" with GNU Make * autogen.sh: Exit on all failures

--- autogen.sh
+++ autogen.sh
@@ -6,15 +6,22 @@ test -z "$srcdir" && srcdir=.
 
 (
 cd $srcdir
-if test -d macros; then
-	test -f mc-aclocal.m4 && test -f gettext.m4 && cat mc-aclocal.m4 gettext.m4 > acinclude.m4
-	aclocal -I macros $ACLOCAL_FLAGS
+if test -d macros && test -f mc-aclocal.m4 && test -f gettext.m4; then
+	cat mc-aclocal.m4 gettext.m4 > acinclude.m4
+	aclocal -I macros $ACLOCAL_FLAGS || exit 1
 else
-	echo macros directory not found, skipping generation of aclocal.m4
+	if test -f aclocal.m4; then
+		echo Warning: aclocal.m4 cannot be rebuilt
+	else
+		echo Error: aclocal.m4 cannot be created
+		exit 1
+	fi
 fi
-autoheader
-autoconf
-(cd vfs/samba; autoheader; autoconf)
-)
+autoheader || exit 1
+autoconf  || exit 1
+cd vfs/samba || exit 1
+autoheader || exit 1
+autoconf || exit 1
+) || exit 1
 
 $srcdir/configure --enable-maintainer-mode $*
