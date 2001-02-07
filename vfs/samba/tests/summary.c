1c3d0e0d6271ce675f94be531001dfba71647c53
Pavel Roskin <proski@gnu.org>
2001-02-07 15:41:18 +0000

    * samba/configure.in: Remove checks for statvfs and friends - they are irrelevant to the client code. * samba/tests/summary.c: Likewise.

--- vfs/samba/tests/summary.c
+++ vfs/samba/tests/summary.c
@@ -19,11 +19,6 @@ main()
 	printf("WARNING: No automated netmask determination - use an interfaces line\n");
 #endif
 
-#if !(defined(STAT_STATVFS) || defined(STAT_STATVFS64) || defined(STAT_STATFS3_OSF1) || defined(STAT_STATFS2_BSIZE) || defined(STAT_STATFS4) || defined(STAT_STATFS2_FSIZE) || defined(STAT_STATFS2_FS_DATA))
-	printf("ERROR: No disk free routine!\n");
-	exit(1);
-#endif
-
 #if !((defined(HAVE_RANDOM) || defined(HAVE_RAND)) && (defined(HAVE_SRANDOM) || defined(HAVE_SRAND)))
     printf("ERROR: No random or srandom routine!\n");
     exit(1);
