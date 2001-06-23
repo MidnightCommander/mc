89ce46f06395f02095bac5a9f03bf0b307ef4f73
Pavel Roskin <proski@gnu.org>
2001-06-23 03:24:34 +0000

    * extraconf.h: Don't define HAVE_INFOMOUNT_QNX on QNX Neutrino.

--- extraconf.h
+++ extraconf.h
@@ -67,7 +67,7 @@ the need to embed this logic into configure.in.
 #    undef USE_EXT2FSLIB
 #endif
 
-#if defined (__QNX__) && !defined (HAVE_INFOMOUNT_LIST)
+#if defined (__QNX__) && !defined(__QNXNTO__) && !defined (HAVE_INFOMOUNT_LIST)
 #    define HAVE_INFOMOUNT_QNX
 #endif
 
