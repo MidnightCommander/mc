5f2c090d3e2ffcf828478a5839589f8901a159db
Pavel Roskin <proski@gnu.org>
2001-11-29 02:06:58 +0000

    This directory is now dead.

--- gnome/gprefs.h
+++
@@ -1,8 +0,0 @@
-#ifndef __GPREFS_H__
-#define __GPREFS_H__
-
-void gnome_configure_box (GtkWidget *widget, WPanel *panel);
-void gnome_configure_box_with_desktop (GtkWidget *widget, WPanel *panel, gboolean desktop);
-
-
-#endif
--- gnome/gwidget.h
+++
@@ -1,6 +0,0 @@
-#ifndef __GWIDGET_H
-#define __GWIDGET_H
-
-char *stock_from_text (char *text);
-
-#endif /* __GWIDGET_H */
