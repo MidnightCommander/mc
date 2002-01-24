f3ca861b8fb373cd56af7471406c654ae69d4729
Pavel Roskin <proski@gnu.org>
2002-01-24 15:51:57 +0000

    * cpio.c: Don't include glib.h directly.

--- vfs/cpio.c
+++ vfs/cpio.c
@@ -20,7 +20,6 @@
    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #include <config.h>
-#include <glib.h>
 #include <errno.h>
 #include "utilvfs.h"
 #include "xdirentry.h"
