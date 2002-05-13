16b73c1f390fcf3b72f516971bbc610dfb23722d
Andrew V. Samoilov <sav@bcs.zp.ua>
2002-05-13 17:12:45 +0000

    * editcmd.c (edit_save_file): Call mc_chown () and mc_chmod () before mc_open () to prevent hangs over ftpfs.

--- edit/editcmd.c
+++ edit/editcmd.c
@@ -222,12 +222,12 @@ int edit_save_file (WEdit * edit, const char *filename)
     } else
 	savename = g_strdup (filename);
 
+    mc_chmod (savename, edit->stat1.st_mode);
+    mc_chown (savename, edit->stat1.st_uid, edit->stat1.st_gid);
+
     if ((fd = mc_open (savename, O_CREAT | O_WRONLY | O_TRUNC | MY_O_TEXT,
 		    edit->stat1.st_mode)) == -1)
 	goto error_save;
-
-    mc_chmod (savename, edit->stat1.st_mode);
-    mc_chown (savename, edit->stat1.st_uid, edit->stat1.st_gid);
 
 /* pipe save */
     if ((p = (char *) edit_get_write_filter (savename, filename))) {
