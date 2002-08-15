11a66b3ffd9d1e8f2ded978682999e2911b479f5
Pavel Roskin <proski@gnu.org>
2002-08-15 01:33:11 +0000

    * vfs.h [!USE_VFS]: Declare mc_setctl as static inline function to fix warnings.

--- vfs/vfs.h
+++ vfs/vfs.h
@@ -265,7 +265,7 @@
 #   define mc_munmap munmap
 
 #   define mc_ctl(a,b,c) 0
-#   define mc_setctl(a,b,c) 0
+static inline int mc_setctl(a,b,c) { return 0; }
 #   define vfs_translate_url(s) g_strdup(s)
 
 #   define mc_stat stat
