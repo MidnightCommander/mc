9514de4ed7cce692f650882a75ee0d67fc0f0a39
Pavel Roskin <proski@gnu.org>
2002-08-27 05:27:11 +0000

    * charsets.c: Add disclaimer of warranty. * filenot.c: Likewise. * fileopctx.c: Likewise. * mfmt.c: Likewise. * selcodepage.c: Likewise. * text.c: Likewise. * textconf.c: Likewise.

--- src/fileopctx.c
+++ src/fileopctx.c
@@ -1,9 +1,23 @@
 /* File operation contexts for the Midnight Commander
- *
- * Copyright (C) 1998 The Free Software Foundation
- *
- * Authors: Federico Mena <federico@nuclecu.unam.mx>
- *          Miguel de Icaza <miguel@nuclecu.unam.mx>
+
+   Copyright (C) 1998 The Free Software Foundation
+
+   Authors: Federico Mena <federico@nuclecu.unam.mx>
+            Miguel de Icaza <miguel@nuclecu.unam.mx>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 #include <config.h>
