7f75f866af163f2474c039064bf652cdfc2916de
Andrew V. Samoilov <sav@bcs.zp.ua>
2002-09-13 10:00:23 +0000

    Declare smbfs_set_debugf().

--- vfs/smbfs.h
+++ vfs/smbfs.h
@@ -3,6 +3,7 @@
 #if !defined(__SMBFS_H)
 #define __SMBFS_H
 
-extern void smbfs_set_debug(int arg);
+extern void smbfs_set_debug (int arg);
+extern void smbfs_set_debugf (const char *filename);
 
 #endif /* __SMBFS_H*/
