4d20330abc6636062584a8ce7806954883950ce9
Pavel Roskin <proski@gnu.org>
2002-10-30 23:16:16 +0000

    Reformat.

--- src/selcodepage.h
+++ src/selcodepage.h
@@ -2,8 +2,8 @@
 #ifndef __SELCODEPAGE_H__
 #define __SELCODEPAGE_H__
 
-int select_charset( int current_charset, int seldisplay );
-int do_select_codepage(void);
+int select_charset (int current_charset, int seldisplay);
+int do_select_codepage (void);
 
-#endif /* __SELCODEPAGE_H__ */
-#endif /* HAVE_CHARSET */
+#endif				/* __SELCODEPAGE_H__ */
+#endif				/* HAVE_CHARSET */
