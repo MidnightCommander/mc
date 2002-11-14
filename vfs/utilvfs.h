68fdcd473aee6d421a186efe48c3b3289110332d
Pavel Roskin <proski@gnu.org>
2002-11-14 07:26:32 +0000

    Include wtools.h - needed because message() has been moved there.

--- vfs/utilvfs.h
+++ vfs/utilvfs.h
@@ -1,5 +1,6 @@
 #include "../src/global.h"
 
 #include "../src/tty.h"		/* enable/disable interrupt key */
-#include "../src/background.h"
-#include "../src/main.h"
+#include "../src/wtools.h"	/* MSG_ERROR */
+#include "../src/background.h"  /* message_1s */
+#include "../src/main.h"	/* print_vfs_message */
