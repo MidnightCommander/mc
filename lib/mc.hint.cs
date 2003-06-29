c1d9304c3fe8bd532ed3f2fae3bb24c54573f791
Pavel Roskin <proski@gnu.org>
2003-06-29 23:18:23 +0000

    * lib/mc.hint.cs: Typo fixes.

--- lib/mc.hint.cs
+++ lib/mc.hint.cs
@@ -5,7 +5,7 @@ Tip: Pou¾itím M-p nebo M-n se pohybujete v historii pøíkazù.
 Tip: Chcete zadat speciální znak(napø. tabelátor)? Pou¾ijte Ctrl-Q a daný znak.
 Tip: Nechcete ji¾ vidìt tipy? Vypnìte je v Nastavení/Zobrazení/Dal¹í nastavení.
 Tip: Adresáøe vyberete pøidáním lomítka na konec vzorku.
-Tip: Pokud Vá¹ terminál nezní nebo nemá funkèní klávesy, pou¾ijte Esc+èíslo.
+Tip: Pokud Vá¹ terminál nezná nebo nemá funkèní klávesy, pou¾ijte Esc+èíslo.
 Tip: Domácí stránka programu Midnight Commander je http://www.ibiblio.org/mc/
 Tip: Zprávy o chybách posílejte na mc-devel@gnome.org (anglicky).
 Tip: Pomocí klávesy Tab zmìníte aktuální panel.
@@ -22,12 +22,12 @@ Tip: Pokud chcete vidìt soubory zaèínající teèkou, øeknìte to v Nastavení.
 Tip: Chcete vidìt zálohy svých souborù (konèí ~). Pou¾ijte Nastavení.
 Tip: Doplòování funguje ve vstupních øádkách v¹ech dialogù. Pou¾ijte M-Tab.
 Tip: Na terminálech s pomalou odezvou skuste na parametr -s (mc -s).
-Tip: S nalezenými soubory mù¾ete pracovat. Stiknìte tlaèítko Panelizace.
+Tip: S nalezenými soubory mù¾ete pracovat. Stisknìte tlaèítko Panelizace.
 Tip: Pro opravdu slo¾ité operace zkuste pøíkaz Pøíkaz/Externí panelizace.
 Tip: Pokud chcete zmìnit adresáø pøi práci na pøíkoazové øádce, zkuste M-c.
 Poznámka: Pøíkazy shellu nefungují, pokud nepracujete na lokální systému souborù.
 Tip: Smazaný text obnovíte C-y.
-Tip: Pokud nìkteré klávesy nefunfují, pou¾ijte menu Nastavení/uèení kláves.
+Tip: Pokud nìkteré klávesy nefungují, pou¾ijte menu Nastavení/uèení kláves.
 Tip: F13 (nebo Shift-F3) vyvolá prohlí¾ení bez zpracování obsahu souboru.
 Tip: Pokud v xtermu nepracují klávesy, zkuste soubory xterm.ad, .ti a .tcap.
 Tip: Editor pro klávesu F4 urèíte pomocí promìnné prostøedí EDITOR.
