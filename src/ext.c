452de6619d54e3750d25e43c4db97f0d523eebc0
Pavel Roskin <proski@gnu.org>
2003-07-02 16:42:36 +0000

    * ext.c (regex_check_type): Set content_shift to skip the filename.

--- src/ext.c
+++ src/ext.c
@@ -419,6 +419,7 @@ regex_check_type (char *filename, int file_len, char *ptr, int *have_type)
 	    if (islocal) {
 		if (!strncmp (content_string, filename, file_len)) {
 		    /* Skip "filename: " */
+		    content_shift = file_len;
 		    if (content_string[content_shift] == ':') {
 			content_shift++;
 			/* Solaris' file prints tab(s) after ':' */
