bf5a5c4e0434bffc92f2105a0a9773c67fdaa73e
Pavel Roskin <proski@gnu.org>
2003-10-15 07:05:24 +0000

    * glibcompat.h: Define g_try_malloc as malloc for glib 1.2.x.

--- src/glibcompat.h
+++ src/glibcompat.h
@@ -20,6 +20,7 @@
 
 #if GLIB_MAJOR_VERSION < 2
 gsize g_strlcpy (gchar *dest, const gchar *src, gsize dest_size);
+#define g_try_malloc(x) malloc(x)
 #endif				/* GLIB_MAJOR_VERSION < 2 */
 
 #endif				/* !__MC_GLIBCOMPAT_H */
