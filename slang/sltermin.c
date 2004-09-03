ef67afe62d83a11f7312fdc179f8e6d3fdcd9711
Andrew V. Samoilov <sav@bcs.zp.ua>
2004-09-03 22:08:20 +0000

    * sltermin.c (): Avoid strncpy, home_ti is 1K long and         nobody really needs it whole cleared

--- slang/sltermin.c
+++ slang/sltermin.c
@@ -267,9 +267,12 @@ SLterminfo_Type *_SLtt_tigetent (char *term)
 
    if (NULL != (home = getenv ("HOME")))
      {
-	strncpy (home_ti, home, sizeof (home_ti) - 11);
-	home_ti [sizeof(home_ti) - 11] = 0;
-	strcat (home_ti, "/.terminfo");
+	size_t len = strlen (home);
+
+	if (len > sizeof (home_ti) - sizeof ("/.terminfo"))
+	    len = sizeof (home_ti) - sizeof ("/.terminfo");
+	memcpy (home_ti, home, len);
+	memcpy (home_ti + len, "/.terminfo", sizeof ("/.terminfo"));
 	Terminfo_Dirs [0] = home_ti;
      }
 
