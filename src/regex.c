fa72520d74c23ea038d2bdae5936cdc019ef32a6
Roland Illig <roland.illig@gmx.de>
2004-10-22 22:53:37 +0000

    * regex.c (regerror): Never use the __memcpy function, as it 	breaks the build on Cygwin.

--- src/regex.c
+++ src/regex.c
@@ -8243,7 +8243,7 @@ regerror (errcode, preg, errbuf, errbuf_size)
     {
       if (msg_size > errbuf_size)
         {
-#if defined HAVE_MEMPCPY || defined _LIBC
+#if 0 && (defined HAVE_MEMPCPY || defined _LIBC)
 	  *((char *) __mempcpy (errbuf, msg, errbuf_size - 1)) = '\0';
 #else
           memcpy (errbuf, msg, errbuf_size - 1);
