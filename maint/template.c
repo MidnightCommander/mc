a8de548480f40d1daf9c7028f9dfc99022da3812
Roland Illig <roland.illig@gmx.de>
2005-02-07 00:06:16 +0000

    Added a template for new C files.

---
+++ maint/template.c
@@ -0,0 +1,44 @@
+/* TODO: one line comment what the file is for.
+
+   Copyright (C) 2005 The Free Software Foundation
+
+   Written by:
+   AUTHOR <MAIL>, YEARS.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <...>
+
+#include "../src/global.h"
+#include "..."
+
+/*** global variables **************************************************/
+
+/*** file scope macro definitions **************************************/
+
+/*** file scope type declarations **************************************/
+
+/*** file scope variables **********************************************/
+
+/*** file scope functions **********************************************/
+
+/*** public functions **************************************************/
