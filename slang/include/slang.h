dbd60622731bedb545bb7492ca156eafdec2616c
Roland Illig <roland.illig@gmx.de>
2005-02-10 13:29:51 +0000

    * include/slang.h: Define __unix__ if __NetBSD__ is defined.

--- slang/include/slang.h
+++ slang/include/slang.h
@@ -24,7 +24,7 @@
 # endif
 #endif /* __watcomc__ */
 
-#if defined(unix) || defined(__unix) || defined (_AIX)
+#if defined(unix) || defined(__unix) || defined (_AIX) || defined(__NetBSD__)
 # ifndef __unix__
 #  define __unix__ 1
 # endif
