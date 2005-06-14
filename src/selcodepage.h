0979ed9626b6dcd4d34e47d143d3f5d7884201ca
Roland Illig <roland.illig@gmx.de>
2005-06-14 13:14:49 +0000

    Changed the name of the multiple-inclusion guard.

--- src/selcodepage.h
+++ src/selcodepage.h
@@ -1,9 +1,9 @@
-#ifdef HAVE_CHARSET
-#ifndef __SELCODEPAGE_H__
-#define __SELCODEPAGE_H__
+#ifndef MC_SELCODEPAGE_H
+#define MC_SELCODEPAGE_H
 
+#ifdef HAVE_CHARSET
 int select_charset (int current_charset, int seldisplay);
 int do_select_codepage (void);
-
-#endif				/* __SELCODEPAGE_H__ */
 #endif				/* HAVE_CHARSET */
+
+#endif
