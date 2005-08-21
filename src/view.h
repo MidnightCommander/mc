03bdc998cacf1b4cc0f24f43c352e3980acc07df
Roland Illig <roland.illig@gmx.de>
2005-08-21 03:32:17 +0000

    * view.h: Rewrote it. All functions are preceded by a describing 	comment and the "extern" keyword.

--- src/view.h
+++ src/view.h
@@ -1,20 +1,25 @@
 #ifndef MC_VIEW_H
 #define MC_VIEW_H
 
-struct WView;			/* opaque structure, can be cast to Widget */
-typedef struct WView WView;
+typedef struct WView WView;	/* Can be cast to Widget */
 
-/* Creation/initialization of a new view widget */
-WView *view_new (int y, int x, int cols, int lines, int is_panel);
-int view_load (WView *view, const char *_command, const char *_file,
-	       int start_line);
+/* Creates a new WView object with the given properties. Caveat: the
+ * origin is in y-x order, while the extent is in x-y order. */
+extern WView *view_new (int y, int x, int cols, int lines, int is_panel);
 
-/* View a ''file'' or the output of a ''command'' in the internal viewer,
- * starting in line ''start_line''. ''ret_move_direction'' may be NULL or
+/* If {command} is not NULL, loads the output of the shell command
+ * {command} and ignores {file}. If {command} is NULL, loads the
+ * {file}. If the {file} is also NULL, loads nothing. If {start_line}
+ * is positive, the output is shown starting in that line. */
+extern int view_load (WView *view, const char *command, const char *file,
+	int start_line);
+
+/* Shows {file} or the output of {command} in the internal viewer,
+ * starting in line {start_line}. {ret_move_direction} may be NULL or
  * point to a variable that will receive the direction in which the user
  * wants to move (-1 = previous file, 1 = next file, 0 = do nothing).
  */
-int mc_internal_viewer (const char *command, const char *file,
+extern int mc_internal_viewer (const char *command, const char *file,
 	int *ret_move_direction, int start_line);
 
 extern int mouse_move_pages_viewer;
