ba9bd33681b2a9e17112d088310a490e40293b6e
Roland Illig <roland.illig@gmx.de>
2006-02-03 14:42:36 +0000

    * view.c: s/attrset/tty_setcolor/g

--- src/wtools.h
+++ src/wtools.h
@@ -66,11 +66,11 @@ void query_set_sel (int new_sel);
 /* Create message box but don't dismiss it yet, not background safe */
 struct Dlg_head *create_message (int flags, const char *title,
 				 const char *text, ...)
-    __attribute__ ((format (printf, 3, 4)));
+    __attribute__ ((format (__printf__, 3, 4)));
 
 /* Show message box, background safe */
 void message (int flags, const char *title, const char *text, ...)
-    __attribute__ ((format (printf, 3, 4)));
+    __attribute__ ((format (__printf__, 3, 4)));
 
 
 /* Use this as header for message() - it expands to "Error" */
