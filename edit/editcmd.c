e335712e41a9bc485b87792f248f15db33b51eca
Slava Zanko <slavazanko@gmail.com>
2009-06-12 14:36:30 +0300

    Fix path to Syntax file
    Signed-off-by: Slava Zanko <slavazanko@gmail.com>

--- edit/editcmd.c
+++ edit/editcmd.c
@@ -814,7 +814,12 @@ edit_load_syntax_file (WEdit * edit)
 			    _(" Which syntax file you want to edit? "), D_NORMAL, 2,
 			    _("&User"), _("&System Wide"));
     }
-    extdir = concat_dir_and_file (mc_home_alt, "syntax" PATH_SEP_STR "Syntax");
+
+    extdir = concat_dir_and_file (mc_home, "syntax" PATH_SEP_STR "Syntax");
+    if (!exist_file(extdir)) {
+	g_free (extdir);
+	extdir = concat_dir_and_file (mc_home_alt, "syntax" PATH_SEP_STR "Syntax");
+    }
 
     if (dir == 0) {
 	char *buffer;
