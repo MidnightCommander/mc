f114fd4135b32b62b51d5530afdc8e9fa6bf6885
Slava Zanko <slavazanko@gmail.com>
2009-07-16 13:25:26 +0300

    Avoid warning in src/search/regex.c about signed and unsigned types
    Signed-off-by: Slava Zanko <slavazanko@gmail.com>

--- src/search/regex.c
+++ src/search/regex.c
@@ -428,7 +428,7 @@ mc_search_regex__process_append_str (GString * dest_str, const char *from, gsize
     char *tmp_str;
     GString *tmp_string;
 
-    if (len == -1)
+    if (len == (gsize) -1)
         len = strlen (from);
 
     if (*replace_flags == REPLACE_T_NO_TRANSFORM) {
