f6637e487108da18dc77c16e3fd76e4e4cadda07
Enrico Weigelt, metux IT service <weigelt@metux.de>
2009-11-02 04:55:29 +0100

    Ticket #1780: removing own declaration of errno and strerror()

--- vfs/samba/include/includes.h
+++ vfs/samba/include/includes.h
@@ -454,18 +454,9 @@
 #define MAX(a,b) ((a)>(b)?(a):(b))
 #endif
 
-#ifndef HAVE_STRERROR
-extern char *sys_errlist[];
-#define strerror(i) sys_errlist[i]
-#endif
-
 #ifndef HAVE_STRCHR
 # define strchr                 index
 # define strrchr                rindex
-#endif
-
-#ifndef HAVE_ERRNO_DECL
-extern int errno;
 #endif
 
 #ifdef HAVE_BROKEN_GETGROUPS
