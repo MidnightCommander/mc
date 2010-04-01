0731ad5e60e69fffae55a77e5b92b4867236ee3b
Andrew Borodin <aborodin@vmail.ru>
2010-04-01 13:14:00 +0400

    Applied MC indentation policy.
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- src/help.h
+++ src/help.h
@@ -30,17 +30,17 @@
 #define MC_HELP_H
 
 /* Markers used in the help files */
-#define CHAR_LINK_START		'\01'	/* Ctrl-A */
-#define CHAR_LINK_POINTER	'\02'	/* Ctrl-B */
-#define CHAR_LINK_END		'\03'	/* Ctrl-C */
-#define CHAR_NODE_END		'\04'	/* Ctrl-D */
-#define CHAR_ALTERNATE		'\05'	/* Ctrl-E */
-#define CHAR_NORMAL		'\06'	/* Ctrl-F */
-#define CHAR_VERSION		'\07'	/* Ctrl-G */
-#define CHAR_FONT_BOLD		'\010'	/* Ctrl-H */
-#define CHAR_FONT_NORMAL	'\013'	/* Ctrl-K */
-#define CHAR_FONT_ITALIC	'\024'	/* Ctrl-T */
+#define CHAR_LINK_START	        '\01'   /* Ctrl-A */
+#define CHAR_LINK_POINTER       '\02'   /* Ctrl-B */
+#define CHAR_LINK_END           '\03'   /* Ctrl-C */
+#define CHAR_NODE_END           '\04'   /* Ctrl-D */
+#define CHAR_ALTERNATE          '\05'   /* Ctrl-E */
+#define CHAR_NORMAL             '\06'   /* Ctrl-F */
+#define CHAR_VERSION            '\07'   /* Ctrl-G */
+#define CHAR_FONT_BOLD          '\010'  /* Ctrl-H */
+#define CHAR_FONT_NORMAL        '\013'  /* Ctrl-K */
+#define CHAR_FONT_ITALIC        '\024'  /* Ctrl-T */
 
 void interactive_display (const char *filename, const char *node);
 
-#endif
+#endif /* MC_HELP_H */
