918afd4df192e73ef2aff99e6106bedd6295ab71
Andrew Borodin <aborodin@vmail.ru>
2010-04-13 11:15:59 +0400

    Set maximum width of help window to 80 symbols.
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- src/help.c
+++ src/help.c
@@ -71,7 +71,7 @@ const global_keymap_t *help_map;
 
 #define MAXLINKNAME 80
 #define HISTORY_SIZE 20
-#define HELP_WINDOW_WIDTH (COLS - 16)
+#define HELP_WINDOW_WIDTH min(80, COLS - 16)
 
 #define STRING_LINK_START	"\01"
 #define STRING_LINK_POINTER	"\02"
