72144b4895f65aa6ed5b18b7ad83024b352f3ac2
Andrew Borodin <aborodin@vmail.ru>
2010-05-16 16:57:56 +0400

    edit_set_codeset(): new function to set codeset in MC editor.
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- src/editor/edit-impl.h
+++ src/editor/edit-impl.h
@@ -193,6 +193,7 @@ void edit_update_curs_row (WEdit * edit);
 void edit_update_curs_col (WEdit * edit);
 void edit_find_bracket (WEdit * edit);
 int edit_reload_line (WEdit * edit, const char *filename, long line);
+void edit_set_codeset (WEdit *edit);
 
 void edit_block_copy_cmd (WEdit * edit);
 void edit_block_move_cmd (WEdit * edit);
