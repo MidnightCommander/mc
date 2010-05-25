80cc8e98907cad6e66c7ef5957eebddd3d7328d3
Andrew Borodin <aborodin@vmail.ru>
2010-05-25 11:04:08 +0400

    Stop searching mode when tree becomes unfocused.
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- src/tree.c
+++ src/tree.c
@@ -1032,9 +1032,10 @@ tree_callback (Widget *w, widget_msg_t msg, int parm)
 	   selected item */
 
     case WIDGET_UNFOCUS:
-	tree->active = 0;
-	show_tree (tree);
-	return MSG_HANDLED;
+        tree->active = 0;
+        tree->searching = 0;
+        show_tree (tree);
+        return MSG_HANDLED;
 
     case WIDGET_KEY:
 	return tree_key (tree, parm);
