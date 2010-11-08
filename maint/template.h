4a1a758c2592095c758b923a8d06327864d68c5f
Slava Zanko <slavazanko@gmail.com>
2010-11-08 11:47:55 +0200

    maint: added some templates for newly created files
    Signed-off-by: Slava Zanko <slavazanko@gmail.com>

---
+++ maint/template.h
@@ -0,0 +1,16 @@
+#ifndef MC__TEMPLATE_H
+#define MC__TEMPLATE_H
+
+/*** typedefs(not structures) and defined constants **********************************************/
+
+/*** enums ***************************************************************************************/
+
+/*** structures declarations (and typedefs of structures)*****************************************/
+
+/*** global variables defined in .c file *********************************************************/
+
+/*** declarations of public functions ************************************************************/
+
+/*** inline functions ****************************************************************************/
+
+#endif /* MC__TEMPLATE_H */
---
+++ maint/template.m4
@@ -0,0 +1,17 @@
+dnl @synopsis MACRO_NAME(MACRO-PARAM1,[MACRO-PARAM2],...)
+dnl
+dnl <one line comment what the file is for>
+dnl
+dnl This macro takes next arguments:
+dnl  ...
+dnl
+dnl This macro calls:
+dnl
+dnl   AC_SUBST(XX_SOME_SUBSTED_VAR)
+dnl
+dnl @category <CategoryNameHere>
+dnl @author Firstname Secondname <your@email.in.net>
+dnl @version <modified date>
+dnl @license AllPermissive
+dnl @copyright Free Software Foundation, Inc.
+
