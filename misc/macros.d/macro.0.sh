04d54931d0d078fcdc771dbba316d2ecb43803ee
Ilia Maslakov <il.smind@gmail.com>
2011-02-04 13:15:43 +0300

    added examples     added rules for misc/macros.d/*. in the process of installation, example scripts will be copied into share/mc/examples/macros.d
    Signed-off-by: Ilia Maslakov <il.smind@gmail.com>

---
+++ misc/macros.d/macro.0.sh
@@ -0,0 +1,6 @@
+#interactive
+m       make
+        TMPFILE=`mktemp ${MC_TMPDIR:-/tmp}/up.XXXXXX` || exit 1
+        make 2> $TMPFILE
+        mcedit $TMPFILE
+        rm $TMPFILE
---
+++ misc/macros.d/macro.1.sh
@@ -0,0 +1,5 @@
+u       Upper case selection
+        TMPFILE=`mktemp ${MC_TMPDIR:-/tmp}/up.XXXXXX` || exit 1
+        cat %b > $TMPFILE
+        cat $TMPFILE| sed 's/\(.*\)/\U\1/' >%b
+        rm -f $TMPFILE
