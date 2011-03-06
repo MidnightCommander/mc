bc3c848ab10e4ea7556536ad8c095d0c86c3af6d
Ilia Maslakov <il.smind@gmail.com>
2011-03-06 22:51:04 +0000

    Added two macro examples:
        * macro.6.sh - execute selected block
        * macro.7.sh - delete trailing whitespaces
    
    Signed-off-by: Ilia Maslakov <il.smind@gmail.com>

---
+++ misc/macros.d/macro.6.sh
@@ -0,0 +1,4 @@
+e       execute something
+        TMPFILE=`mktemp ${MC_TMPDIR:-/tmp}/up.XXXXXX` || exit 1
+        cat %b > $TMPFILE
+        sh $TMPFILE > %b
---
+++ misc/macros.d/macro.7.sh
@@ -0,0 +1,4 @@
+w       delete trailing whitespace
+        TMPFILE=`mktemp ${MC_TMPDIR:-/tmp}/up.XXXXXX` || exit 1
+        cat %b > $TMPFILE
+        cat $TMPFILE | sed 's/[ \t]*$//' > %b
