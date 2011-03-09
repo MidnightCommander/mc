994254917c05d2d36192118c723ccef634b0cfb0
Slava Zanko <slavazanko@gmail.com>
2011-03-09 11:33:46 +0200

    Added test for checking if library is independ to $(topsrcdir)/src
    Signed-off-by: Slava Zanko <slavazanko@gmail.com>

---
+++ lib/tests/library_independ.c
@@ -0,0 +1,59 @@
+/* libmc - check if library is independ to $(topsrc)/src directory
+
+   Copyright (C) 2011 Free Software Foundation, Inc.
+
+   Written by:
+    Slava Zanko <slavazanko@gmail.com>, 2011
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License
+   as published by the Free Software Foundation; either version 2 of
+   the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#define TEST_SUITE_NAME "lib/library_independ"
+
+#include <check.h>
+
+#include "lib/global.h"
+
+/* --------------------------------------------------------------------------------------------- */
+
+START_TEST (test_library_independ)
+{
+}
+END_TEST
+
+/* --------------------------------------------------------------------------------------------- */
+
+int
+main (void)
+{
+    int number_failed;
+
+    Suite *s = suite_create (TEST_SUITE_NAME);
+    TCase *tc_core = tcase_create ("Core");
+    SRunner *sr;
+
+    /* Add new tests here: *************** */
+    tcase_add_test (tc_core, test_library_independ);
+    /* *********************************** */
+
+    suite_add_tcase (s, tc_core);
+    sr = srunner_create (s);
+    srunner_run_all (sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed (sr);
+    srunner_free (sr);
+    return (number_failed == 0) ? 0 : 1;
+}
+
+/* --------------------------------------------------------------------------------------------- */
