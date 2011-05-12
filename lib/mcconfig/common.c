ccd42c0fab9124e72126bd95757367031cff3892
Andrew Borodin <aborodin@vmail.ru>
2011-05-12 15:02:12 +0400

    (mc_config_new_or_override_file): create file w/o O_SYNC flag.
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- lib/mcconfig/common.c
+++ lib/mcconfig/common.c
@@ -62,7 +62,7 @@ mc_config_new_or_override_file (mc_config_t * mc_config, const gchar * ini_path,
     }
     mc_util_make_backup_if_possible (ini_path, "~");
 
-    fd = mc_open (ini_path, O_WRONLY | O_TRUNC | O_SYNC, 0);
+    fd = mc_open (ini_path, O_WRONLY | O_TRUNC, 0);
     if (fd == -1)
     {
         g_propagate_error (error, g_error_new (MC_ERROR, 0, "%s", unix_error_string (errno)));
