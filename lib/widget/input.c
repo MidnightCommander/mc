MidnightCommander/mc

commit cff59255982d470b11a97408d7d3571b03b8401a

Optimization of history save.
Formerly, each widget saved its history self in WIDGET_DESTROY stage.
Thus, history file was read and written as many times as many widgets
with history are in dialog.

Now all widget histories are written to ${XDG_CACHE_HOME}/mc/history
file at one time before dialog destruction.

An ev_history_load_save_t event type is created to use new event engine
to save histories.

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

lib/widget/input.c
@@ -758,6 +758,30 @@ input_execute_cmd (WInput * in, unsigned long command)
 
 /* --------------------------------------------------------------------------------------------- */
 
+/* "history_save" event handler */
+static gboolean
+input_save_history (const gchar * event_group_name, const gchar * event_name,
+                    gpointer init_data, gpointer data)
+{
+    WInput *in = (WInput *) init_data;
+
+    (void) event_group_name;
+    (void) event_name;
+
+    if (in->history != NULL && !in->is_password && (((Widget *) in)->owner->ret_value != B_CANCEL))
+    {
+        ev_history_load_save_t *ev = (ev_history_load_save_t *) data;
+
+        if (in->need_push)
+            push_history (in, in->buffer);
+        history_save (ev->cfg, in->history_name, in->history);
+    }
+
+    return TRUE;
+}
+
+/* --------------------------------------------------------------------------------------------- */
+
 static void
 input_destroy (WInput * in)
 {
@@ -769,11 +793,10 @@ input_destroy (WInput * in)
 
     input_clean (in);
 
+    /* clean history */
     if (in->history != NULL)
     {
-        if (!in->is_password && (((Widget *) in)->owner->ret_value != B_CANCEL))
-            history_put (in->history_name, in->history);
-
+        /* history is already saved before this moment */
         in->history = g_list_first (in->history);
         g_list_foreach (in->history, (GFunc) g_free, NULL);
         g_list_free (in->history);
@@ -903,6 +926,11 @@ input_callback (Widget * w, widget_msg_t msg, int parm)
 
     switch (msg)
     {
+    case WIDGET_INIT:
+        /* subscribe to "history_save" event */
+        mc_event_add (w->owner->event_group, MCEVENT_HISTORY_SAVE, input_save_history, w, NULL);
+        return MSG_HANDLED;
+
     case WIDGET_KEY:
         if (parm == XCTRL ('q'))
         {
@@ -943,6 +971,8 @@ input_callback (Widget * w, widget_msg_t msg, int parm)
         return MSG_HANDLED;
 
     case WIDGET_DESTROY:
+        /* unsubscribe from "history_save" event */
+        mc_event_del (w->owner->event_group, MCEVENT_HISTORY_SAVE, input_save_history, w);
         input_destroy (in);
         return MSG_HANDLED;
 
@@ -1210,7 +1240,8 @@ input_disable_update (WInput * in)
 void
 input_clean (WInput * in)
 {
-    push_history (in, in->buffer);
+    if (in->need_push)
+        push_history (in, in->buffer);
     in->need_push = TRUE;
     in->buffer[0] = '\0';
     in->point = 0;

lib/event-types.h
@@ -7,15 +7,18 @@
 
 /* Event groups for main modules */
 #define MCEVENT_GROUP_CORE "Core"
+#define MCEVENT_GROUP_DIALOG "Dialog"
 #define MCEVENT_GROUP_DIFFVIEWER "DiffViewer"
 #define MCEVENT_GROUP_EDITOR "Editor"
 #define MCEVENT_GROUP_FILEMANAGER "FileManager"
 #define MCEVENT_GROUP_VIEWER "Viewer"
 
+/* Events */
+#define MCEVENT_HISTORY_SAVE "history_save"
+
 /*** enums ***************************************************************************************/
 
 /*** structures declarations (and typedefs of structures)*****************************************/
-
 
 /* MCEVENT_GROUP_CORE:vfs_timestamp */
 struct vfs_class;
@@ -25,7 +28,6 @@ typedef struct
     gpointer id;
     gboolean ret;
 } ev_vfs_stamp_create_t;
-
 
 /* MCEVENT_GROUP_CORE:vfs_print_message */
 typedef struct
@@ -63,6 +65,14 @@ typedef struct
     } ret;
 } ev_background_parent_call_t;
 
+/* MCEVENT_GROUP_DIALOG:history_save */
+struct mc_config_t;
+struct Widget;
+typedef struct
+{
+    struct mc_config_t *cfg;
+    struct Widget *receiver;    /* NULL means broadcast message */
+} ev_history_load_save_t;
 
 /*** global variables defined in .c file *********************************************************/
 

lib/widget/dialog.c
@@ -24,10 +24,13 @@
 #include <config.h>
 
 #include <ctype.h>
+#include <errno.h>
 #include <stdlib.h>
-#include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>              /* open() */
 
 #include "lib/global.h"
 
@@ -37,6 +40,7 @@
 #include "lib/tty/key.h"
 #include "lib/strutil.h"
 #include "lib/widget.h"
+#include "lib/fileloc.h"        /* MC_HISTORY_FILE */
 #include "lib/event.h"          /* mc_event_raise() */
 
 /*** global variables ****************************************************************************/
@@ -1154,6 +1158,8 @@ run_dlg (Dlg_head * h)
 void
 destroy_dlg (Dlg_head * h)
 {
+    /* if some widgets have history, save all history at one moment here */
+    dlg_save_history (h);
     dlg_broadcast_msg (h, WIDGET_DESTROY, FALSE);
     g_list_foreach (h->widgets, (GFunc) g_free, NULL);
     g_list_free (h->widgets);
@@ -1163,6 +1169,43 @@ destroy_dlg (Dlg_head * h)
     g_free (h);
 
     do_refresh ();
+}
+
+/* --------------------------------------------------------------------------------------------- */
+
+/**
+  * Write history to the ${XDG_CACHE_HOME}/mc/history file
+  */
+void
+dlg_save_history (Dlg_head * h)
+{
+    char *profile;
+    int i;
+
+    if (num_history_items_recorded == 0)        /* this is how to disable */
+        return;
+
+    profile = g_build_filename (mc_config_get_cache_path (), MC_HISTORY_FILE, (char *) NULL);
+    i = open (profile, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+    if (i != -1)
+        close (i);
+
+    /* Make sure the history is only readable by the user */
+    if (chmod (profile, S_IRUSR | S_IWUSR) != -1 || errno == ENOENT)
+    {
+        ev_history_load_save_t event_data;
+
+        event_data.cfg = mc_config_init (profile);
+        event_data.receiver = NULL;
+
+        /* get all histories in dialog */
+        mc_event_raise (h->event_group, MCEVENT_HISTORY_SAVE, &event_data);
+
+        mc_config_save_file (event_data.cfg, NULL);
+        mc_config_deinit (event_data.cfg);
+    }
+
+    g_free (profile);
 }
 
 /* --------------------------------------------------------------------------------------------- */

lib/widget/dialog.h
@@ -190,6 +190,7 @@ int run_dlg (Dlg_head * d);
 void destroy_dlg (Dlg_head * h);
 
 void dlg_run_done (Dlg_head * h);
+void dlg_save_history (Dlg_head * h);
 void dlg_process_event (Dlg_head * h, int key, Gpm_Event * event);
 
 char *dlg_get_title (const Dlg_head * h, size_t len);

lib/widget/history.c
@@ -214,6 +214,9 @@ history_save (struct mc_config_t * cfg, const char *name, GList * h)
     GString *buffer;
     int i;
 
+    if (name == NULL || *name == '\0' || h == NULL)
+        return;
+
     /* go to end of list */
     h = g_list_last (h);
 
@@ -261,6 +264,7 @@ history_save (struct mc_config_t * cfg, const char *name, GList * h)
 
 /* --------------------------------------------------------------------------------------------- */
 
+#if 0
 /**
   * Write the history to the ${XDG_CACHE_HOME}/mc/history file.
  */
@@ -296,6 +300,7 @@ history_put (const char *input_name, GList * h)
 
     g_free (profile);
 }
+#endif
 
 /* --------------------------------------------------------------------------------------------- */
 

lib/widget/history.h
@@ -24,8 +24,10 @@ extern int num_history_items_recorded;
 GList *history_get (const char *input_name);
 /* save history to the mc_config, but don't save config to file */
 void history_save (struct mc_config_t * cfg, const char *name, GList * h);
+#if 0
 /* write history to the ${XDG_CACHE_HOME}/mc/history file */
 void history_put (const char *input_name, GList * h);
+#endif
 /* for repositioning of history dialog we should pass widget to this
  * function, as position of history dialog depends on widget's position */
 char *history_show (GList ** history, Widget * widget);

src/filemanager/layout.c
@@ -810,6 +810,7 @@ set_display_type (int num, panel_view_mode_t type)
     unsigned int the_other = 0; /* Index to the other panel */
     const char *file_name = NULL;       /* For Quick view */
     Widget *new_widget = NULL, *old_widget = NULL;
+    panel_view_mode_t old_type;
     WPanel *the_other_panel = NULL;
 
     if (num >= MAX_VIEWS)
@@ -837,15 +838,13 @@ set_display_type (int num, panel_view_mode_t type)
         cols = w->cols;
         lines = w->lines;
         old_widget = w;
+        old_type = panels[num].type;
 
-        if (panels[num].type == view_listing)
+        if (old_type == view_listing && panel->frame_size == frame_full && type != view_listing)
         {
-            if (panel->frame_size == frame_full && type != view_listing)
-            {
-                cols = COLS - first_panel_size;
-                if (num == 1)
-                    x = first_panel_size;
-            }
+            cols = COLS - first_panel_size;
+            if (num == 1)
+                x = first_panel_size;
         }
     }
 
@@ -897,7 +896,16 @@ set_display_type (int num, panel_view_mode_t type)
     /* We use replace to keep the circular list of the dialog in the */
     /* same state.  Maybe we could just kill it and then replace it  */
     if ((midnight_dlg != NULL) && (old_widget != NULL))
+    {
+        if (old_widget == view_listing)
+        {
+            /* save and write directory history of panel
+             * ... and other histories of midnight_dlg  */
+            dlg_save_history (midnight_dlg);
+        }
+
         dlg_replace_widget (old_widget, new_widget);
+    }
 
     if (type == view_listing)
     {

src/filemanager/panel.c
@@ -1187,6 +1187,28 @@ panel_save_name (WPanel * panel)
 
 /* --------------------------------------------------------------------------------------------- */
 
+/* "history_save" event handler */
+static gboolean
+panel_save_history (const gchar * event_group_name, const gchar * event_name,
+                    gpointer init_data, gpointer data)
+{
+    WPanel *p = (WPanel *) init_data;
+
+    (void) event_group_name;
+    (void) event_name;
+
+    if (p->dir_history != NULL)
+    {
+        ev_history_load_save_t *ev = (ev_history_load_save_t *) data;
+
+        history_save (ev->cfg, p->hist_name, p->dir_history);
+    }
+
+    return TRUE;
+}
+
+/* --------------------------------------------------------------------------------------------- */
+
 static void
 panel_destroy (WPanel * p)
 {
@@ -1203,11 +1225,10 @@ panel_destroy (WPanel * p)
 
     panel_clean_dir (p);
 
-    /* save and clean history */
+    /* clean history */
     if (p->dir_history != NULL)
     {
-        history_put (p->hist_name, p->dir_history);
-
+        /* directory history is already saved before this moment */
         p->dir_history = g_list_first (p->dir_history);
         g_list_foreach (p->dir_history, (GFunc) g_free, NULL);
         g_list_free (p->dir_history);
@@ -2908,6 +2929,11 @@ panel_callback (Widget * w, widget_msg_t msg, int parm)
 
     switch (msg)
     {
+    case WIDGET_INIT:
+        /* subscribe to "history_save" event */
+        mc_event_add (w->owner->event_group, MCEVENT_HISTORY_SAVE, panel_save_history, w, NULL);
+        return MSG_HANDLED;
+
     case WIDGET_DRAW:
         /* Repaint everything, including frame and separator */
         paint_frame (panel);    /* including show_dir */
@@ -2956,6 +2982,8 @@ panel_callback (Widget * w, widget_msg_t msg, int parm)
         return panel_execute_cmd (panel, parm);
 
     case WIDGET_DESTROY:
+        /* unsubscribe from "history_save" event */
+        mc_event_del (w->owner->event_group, MCEVENT_HISTORY_SAVE, panel_save_history, w);
         panel_destroy (panel);
         free_my_statfs ();
         return MSG_HANDLED;

commit 5ffcffbeb6bdda29698bc823e389f5126b13ef08

Optimization of history saving.
Now history is not saved if it was not modified and content of input
line is equal to history top entry. Thanks Francesco Cosoleto
<cosoleto at gmail dot com> for the original patch.

Also fixed a regression introduced in cff59255982d470b11a97408d7d3571b03b8401a:
after CK_HistoryNext and CK_HistoryPrev actions in input line,
the input line content is not put to the top of history.

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

lib/widget/input.c
@@ -83,6 +83,19 @@ static char *kill_buffer = NULL;
 /*** file scope functions ************************************************************************/
 /* --------------------------------------------------------------------------------------------- */
 
+static size_t
+get_history_length (const GList *history)
+{
+    size_t len = 0;
+
+    for (; history != NULL; history = g_list_previous (history))
+        len++;
+
+    return len;
+}
+
+/* --------------------------------------------------------------------------------------------- */
+
 static void
 draw_history_button (WInput * in)
 {
@@ -170,7 +183,10 @@ delete_region (WInput * in, int x_first, int x_last)
 static void
 do_show_hist (WInput * in)
 {
+    size_t len;
     char *r;
+
+    len = get_history_length (in->history);
 
     r = history_show (&in->history, &in->widget);
     if (r != NULL)
@@ -178,6 +194,10 @@ do_show_hist (WInput * in)
         input_assign_text (in, r);
         g_free (r);
     }
+
+    /* Has history cleaned up or not? */
+    if (len != get_history_length (in->history))
+        in->history_changed = TRUE;
 }
 
 /* --------------------------------------------------------------------------------------------- */
@@ -224,7 +244,13 @@ push_history (WInput * in, const char *text)
         strip_password (t, i >= ELEMENTS);
     }
 
-    in->history = list_append_unique (in->history, t);
+    if (in->history == NULL || in->history->data == NULL || strcmp (in->history->data, t) != 0 ||
+        in->history_changed)
+    {
+        in->history = list_append_unique (in->history, t);
+        in->history_changed = TRUE;
+    }
+
     in->need_push = FALSE;
 }
 
@@ -584,6 +610,7 @@ hist_prev (WInput * in)
     if (prev != NULL)
     {
         in->history = prev;
+        in->history_changed = TRUE;
         input_assign_text (in, (char *) prev->data);
         in->need_push = FALSE;
     }
@@ -612,6 +639,7 @@ hist_next (WInput * in)
     else
     {
         in->history = next;
+        in->history_changed = TRUE;
         input_assign_text (in, (char *) next->data);
         in->need_push = FALSE;
     }
@@ -814,13 +842,14 @@ input_save_history (const gchar * event_group_name, const gchar * event_name,
     (void) event_group_name;
     (void) event_name;
 
-    if (in->history != NULL && !in->is_password && (((Widget *) in)->owner->ret_value != B_CANCEL))
+    if (in->history != NULL && in->history_changed && !in->is_password &&
+        (((Widget *) in)->owner->ret_value != B_CANCEL))
     {
         ev_history_load_save_t *ev = (ev_history_load_save_t *) data;
 
-        if (in->need_push)
-            push_history (in, in->buffer);
+        push_history (in, in->buffer);
         history_save (ev->cfg, in->history_name, in->history);
+        in->history_changed = FALSE;
     }
 
     return TRUE;
@@ -942,8 +971,9 @@ input_new (int y, int x, const int *input_colors, int width, const char *def_tex
     in->completion_flags = completion_flags;
 
     /* prepare to history setup */
-    in->history_name = NULL;
     in->history = NULL;
+    in->history_changed = FALSE;
+    in->history_name = NULL;
     if ((histname != NULL) && (*histname != '\0'))
         in->history_name = g_strdup (histname);
 
@@ -1276,12 +1306,15 @@ input_disable_update (WInput * in)
 
 /* --------------------------------------------------------------------------------------------- */
 
-/* Cleans the input line and adds the current text to the history */
+/**
+  *  Cleans the input line and adds the current text to the history
+  *
+  *  @param in the input line
+  */
 void
 input_clean (WInput * in)
 {
-    if (in->need_push)
-        push_history (in, in->buffer);
+    push_history (in, in->buffer);
     in->need_push = TRUE;
     in->buffer[0] = '\0';
     in->point = 0;

lib/widget/input.h
@@ -60,6 +60,7 @@ typedef struct
     char *buffer;               /* pointer to editing buffer */
     char *history_name;         /* name of history for loading and saving */
     GList *history;             /* the history */
+    gboolean history_changed;   /* the history has changed */
     gboolean need_push;         /* need to push the current Input on hist? */
     char **completions;         /* possible completions array */
     input_complete_t completion_flags;