fec5cd6c5dbb665ca2cdcab3150090923d85be40
Slava Zanko <slavazanko@gmail.com>
2011-10-04 14:04:48 +0300

    Added function vfs_path_cmp()
    Signed-off-by: Slava Zanko <slavazanko@gmail.com>

---
+++ tests/lib/vfs/path_len.c
@@ -0,0 +1,109 @@
+/* lib/vfs - vfs_path_t compare functions
+
+   Copyright (C) 2011 Free Software Foundation, Inc.
+
+   Written by:
+    Slava Zanko <slavazanko@gmail.com>, 2011
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License
+   as published by the Free Software Foundation; either version 2 of
+   the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#define TEST_SUITE_NAME "/lib/vfs"
+
+#include <check.h>
+
+#include "lib/global.c"
+
+#ifndef HAVE_CHARSET
+#define HAVE_CHARSET 1
+#endif
+
+#include "lib/charsets.h"
+
+#include "lib/strutil.h"
+#include "lib/vfs/xdirentry.h"
+#include "lib/vfs/path.h"
+
+#include "src/vfs/local/local.c"
+
+
+static void
+setup (void)
+{
+    str_init_strings (NULL);
+
+    vfs_init ();
+    init_localfs ();
+    vfs_setup_work_dir ();
+
+    mc_global.sysconfig_dir = (char *) TEST_SHARE_DIR;
+    load_codepages_list ();
+}
+
+static void
+teardown (void)
+{
+    vfs_shut ();
+    str_uninit_strings ();
+}
+
+/* --------------------------------------------------------------------------------------------- */
+#define path_len_one_check(input,etalon) {\
+    vpath = vfs_path_from_str (input);\
+    result = vfs_path_len (vpath);\
+    vfs_path_free (vpath); \
+    fail_unless ( result == etalon, "\ninput: %s\nexpected: %d\nactual: %d\n",\
+        input, etalon, result); \
+}
+
+START_TEST (test_path_length)
+{
+    vfs_path_t *vpath;
+    size_t result;
+
+    path_len_one_check ("/тестовый/путь", 26);
+
+    path_len_one_check ("/#enc:KOI8-R/тестовый/путь",  38);
+    path_len_one_check (NULL, 0);
+}
+END_TEST
+
+/* --------------------------------------------------------------------------------------------- */
+
+int
+main (void)
+{
+    int number_failed;
+
+    Suite *s = suite_create (TEST_SUITE_NAME);
+    TCase *tc_core = tcase_create ("Core");
+    SRunner *sr;
+
+    tcase_add_checked_fixture (tc_core, setup, teardown);
+
+    /* Add new tests here: *************** */
+    tcase_add_test (tc_core, test_path_length);
+    /* *********************************** */
+
+    suite_add_tcase (s, tc_core);
+    sr = srunner_create (s);
+    srunner_set_log (sr, "path_len.log");
+    srunner_run_all (sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed (sr);
+    srunner_free (sr);
+    return (number_failed == 0) ? 0 : 1;
+}
+
+/* --------------------------------------------------------------------------------------------- */
