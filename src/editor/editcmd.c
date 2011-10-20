056200ee42f175c098c0ff96946881e6e6c6a934
Slava Zanko <slavazanko@gmail.com>
2011-10-20 16:12:02 +0300

    Fixed searching the start of word
    Signed-off-by: Slava Zanko <slavazanko@gmail.com>

--- src/editor/editcmd.c
+++ src/editor/editcmd.c
@@ -1100,7 +1100,7 @@ pipe_mail (WEdit * edit, char *to, char *subject, char *cc)
 static gboolean
 is_break_char (char c)
 {
-    return (isspace (c) || strchr ("{}[]()<>=|/\\!?~'\",.;:#$%^&*", c));
+    return (isspace (c) || strchr ("{}[]()<>=|/\\!?~-+`'\",.;:#$%^&*", c));
 }
 
 /* --------------------------------------------------------------------------------------------- */
