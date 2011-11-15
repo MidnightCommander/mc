6704d927c026f20d018672f4c23c5a0ebf1da235
Andrew Borodin <aborodin@vmail.ru>
2011-11-15 16:00:59 +0300

    (tty_lowlevel_getch): used in lib/tty only.
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- lib/tty/tty.h
+++ lib/tty/tty.h
@@ -97,8 +97,6 @@ extern void tty_keypad (gboolean set);
 extern void tty_nodelay (gboolean set);
 extern int tty_baudrate (void);
 
-extern int tty_lowlevel_getch (void);
-
 /* {{{ Output }}} */
 
 /*
