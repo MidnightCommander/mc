MidnightCommander/mc

commit c457eaf9b152180f5ce462481146a16a9b19722d

Ticket #2776: file selection reset after exit from the archive in the root.
After exit from archive located in the root of FS, file selection is
reset (/bin is selected instead of the archive).

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

src/filemanager/panel.c
@@ -2980,10 +2980,15 @@ get_parent_dir_name (const char *cwd, const char *lwd)
         return NULL;
     }
 
-    while (--p > lwd && *p != PATH_SEP);
-    while (--p > lwd && *p != PATH_SEP);
+    /* skip VFS prefix */
+    while (--p > lwd && *p != PATH_SEP)
+        ;
+    /* get last component */
+    while (--p > lwd && *p != PATH_SEP)
+        ;
 
-    return (p != lwd) ? p + 1 : NULL;
+    /* return last component */
+    return (*p == PATH_SEP) ? p + 1 : p;
 }
 
 /* --------------------------------------------------------------------------------------------- */

commit 638cac9be3c2418728ad4670dec2f5bcf2791f19

Ticket #2776: file selection reset after cd to ".." in remote VFS.
After cd from directory to ".." in remote VFS, directory selection is
reset (".." is selected instead of the previous directory).

Bug was introduced in c457eaf9b152180f5ce462481146a16a9b19722d.

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

src/filemanager/panel.c
@@ -2988,7 +2988,7 @@ get_parent_dir_name (const char *cwd, const char *lwd)
         ;
 
     /* return last component */
-    return (*p == PATH_SEP) ? p + 1 : p;
+    return (p != lwd || *p == PATH_SEP) ? p + 1 : p;
 }
 
 /* --------------------------------------------------------------------------------------------- */