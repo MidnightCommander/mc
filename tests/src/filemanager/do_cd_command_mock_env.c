84fecff024d548d4ec217061f8d7677e05bce66a
Slava Zanko <slavazanko@gmail.com>
2012-12-18 15:00:03 +0300

    Ticket #2883: non-portable test
    Test has been rewritten for support actuality.
    
    Signed-off-by: Slava Zanko <slavazanko@gmail.com>

---
+++ tests/src/filemanager/do_cd_command_mock_env.c
@@ -0,0 +1,46 @@
+#include "src/setup.h"
+
+WPanel *current_panel;
+gboolean command_prompt;
+enum subshell_state_enum subshell_state;
+int quit;
+
+void
+sync_tree (const char *path)
+{
+    (void) path;
+}
+
+gboolean
+quiet_quit_cmd (void)
+{
+    return TRUE;
+}
+
+char *
+expand_format (struct WEdit *edit_widget, char c, gboolean do_quote)
+{
+    (void) edit_widget;
+    (void) c;
+    (void) do_quote;
+
+    return NULL;
+}
+
+void
+shell_execute (const char *command, int flags)
+{
+    (void) command;
+    (void) flags;
+}
+
+void
+init_subshell (void)
+{
+}
+
+gboolean
+do_load_prompt (void)
+{
+    return TRUE;
+}
