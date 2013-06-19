90209d3097c54da50c02e641ef3405de9acfbc0c
Slava Zanko <slavazanko@gmail.com>
2013-06-19 22:56:55 +0300

    Code indentation
    Signed-off-by: Slava Zanko <slavazanko@gmail.com>

--- src/vfs/sftpfs/vfs_subclass.c
+++ src/vfs/sftpfs/vfs_subclass.c
@@ -27,7 +27,7 @@
 #include <config.h>
 #include <errno.h>
 #include <stdlib.h>
-#include <string.h>     /* memset() */
+#include <string.h>             /* memset() */
 
 #include "lib/global.h"
 #include "lib/vfs/utilvfs.h"
