d74be13ef04d99772fe3dbf7f2516dc8371ef0e2
Andreas Mohr <and@gmx.li>
2015-04-11 12:39:06 +0300

    (file_date): fix variable initialization.
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- lib/timefmt.c
+++ lib/timefmt.c
@@ -120,7 +120,7 @@ const char *
 file_date (time_t when)
 {
     static char timebuf[MB_LEN_MAX * MAX_I18NTIMELENGTH + 1];
-    time_t current_time = time ((time_t) 0);
+    time_t current_time = time (NULL);
     const char *fmt;
 
     if (current_time > when + 6L * 30L * 24L * 60L * 60L        /* Old. */
