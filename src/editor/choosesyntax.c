edff1cff6fff1d9a0ca590d18bd2b0f49cbe2dd7
Andreas Mohr <and@gmx.li>
2016-02-20 12:29:59 +0000

    src/editor/choosesyntax.c: cleanup -Wcast-qual warning.
    Signed-off-by: Andreas Mohr <and@gmx.li>
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- src/editor/choosesyntax.c
+++ src/editor/choosesyntax.c
@@ -61,7 +61,7 @@
 static int
 pstrcmp (const void *p1, const void *p2)
 {
-    return strcmp (*(char **) p1, *(char **) p2);
+    return strcmp (*(char *const *) p1, *(char *const *) p2);
 }
 
 /* --------------------------------------------------------------------------------------------- */
