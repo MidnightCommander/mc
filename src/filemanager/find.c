f3bcc33f917ffd51a350b8a0811ff12c004c3417
Andreas Mohr <and@gmx.li>
2016-09-17 12:05:30 +0000

    src/filemanager/find.c: cleanup -Wformat-signedness warning.
    find.c: In function 'found_num_update':
    find.c:373:44: error: format '%ld' expects argument of type 'long int', but argument 4 has type 'long unsigned int' [-Werror=format=]
         g_snprintf (buffer, sizeof (buffer), _("Found: %ld"), matches);
                                                ^
    
    find.c: In function 'do_search':
    find.c:1306:51: error: format '%zd' expects argument of type 'signed size_t', but argument 4 has type 'size_t {aka long unsigned int}' [-Werror=format=]
                                                       "Finished (ignored %zd directories)",
                                                       ^
    find.c:1305:51: error: format '%zd' expects argument of type 'signed size_t', but argument 4 has type 'size_t {aka long unsigned int}' [-Werror=format=]
                                             ngettext ("Finished (ignored %zd directory)",
                                                       ^
    
    file.c:2526:41: error: format '%zd' expects argument of type 'signed size_t', but argument 3 has type 'size_t {aka long unsigned int}' [-Werror=format=]
         label_set_textv (dsm->count_size, _("Directories: %zd, total size: %s"),
                                             ^
    
    Signed-off-by: Andreas Mohr <and@gmx.li>
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- src/filemanager/find.c
+++ src/filemanager/find.c
@@ -370,7 +370,8 @@ static void
 found_num_update (void)
 {
     char buffer[BUF_TINY];
-    g_snprintf (buffer, sizeof (buffer), _("Found: %ld"), matches);
+
+    g_snprintf (buffer, sizeof (buffer), _("Found: %lu"), matches);
     label_set_text (found_num_label, buffer);
 }
 
@@ -1301,9 +1302,10 @@ do_search (WDialog * h)
                         else
                         {
                             char msg[BUF_SMALL];
+
                             g_snprintf (msg, sizeof (msg),
-                                        ngettext ("Finished (ignored %zd directory)",
-                                                  "Finished (ignored %zd directories)",
+                                        ngettext ("Finished (ignored %zu directory)",
+                                                  "Finished (ignored %zu directories)",
                                                   ignore_count), ignore_count);
                             status_update (msg);
                         }
