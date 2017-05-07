MidnightCommander/mc

commit 4d65a731c28d53a536a044a85e82223a7aa46bd5

mcview: refactoring of mcview_get_utf().
Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

src/viewer/ascii.c
@@ -354,11 +354,9 @@ mcview_get_next_char (WView * view, mcview_state_machine_t * state, int *c)
 #ifdef HAVE_CHARSET
     if (view->utf8)
     {
-        gboolean result;
-        int char_length;
+        int char_length = 0;
 
-        *c = mcview_get_utf (view, state->offset, &char_length, &result);
-        if (!result)
+        if (!mcview_get_utf (view, state->offset, c, &char_length))
             return FALSE;
         /* Pretend EOF if we crossed force_max */
         if (view->force_max >= 0 && state->offset + char_length > view->force_max)

src/viewer/datasource.c
@@ -151,17 +151,12 @@ mcview_get_ptr_file (WView * view, off_t byte_index)
 
 /* --------------------------------------------------------------------------------------------- */
 
-int
-mcview_get_utf (WView * view, off_t byte_index, int *char_length, gboolean * result)
+gboolean
+mcview_get_utf (WView * view, off_t byte_index, int *ch, int *ch_len)
 {
     gchar *str = NULL;
-    int res = -1;
-    gunichar ch;
-    gchar *next_ch = NULL;
+    int res;
     gchar utf8buf[UTF8_CHAR_LEN + 1];
-
-    *char_length = 0;
-    *result = FALSE;
 
     switch (view->datasource)
     {
@@ -180,8 +175,10 @@ mcview_get_utf (WView * view, off_t byte_index, int *char_length, gboolean * res
         break;
     }
 
+    *ch = 0;
+
     if (str == NULL)
-        return 0;
+        return FALSE;
 
     res = g_utf8_get_char_validated (str, -1);
 
@@ -189,6 +186,7 @@ mcview_get_utf (WView * view, off_t byte_index, int *char_length, gboolean * res
     {
         /* Retry with explicit bytes to make sure it's not a buffer boundary */
         int i;
+
         for (i = 0; i < UTF8_CHAR_LEN; i++)
         {
             if (mcview_get_byte (view, byte_index + i, &res))
@@ -206,18 +204,20 @@ mcview_get_utf (WView * view, off_t byte_index, int *char_length, gboolean * res
 
     if (res < 0)
     {
-        ch = *str;
-        *char_length = 1;
+        *ch = (unsigned char) (*str);
+        *ch_len = 1;
     }
     else
     {
-        ch = res;
+        gchar *next_ch = NULL;
+
+        *ch = res;
         /* Calculate UTF-8 char length */
         next_ch = g_utf8_next_char (str);
-        *char_length = next_ch - str;
+        *ch_len = next_ch - str;
     }
-    *result = TRUE;
-    return ch;
+
+    return TRUE;
 }
 
 /* --------------------------------------------------------------------------------------------- */

src/viewer/internal.h
@@ -264,7 +264,7 @@ off_t mcview_get_filesize (WView *);
 void mcview_update_filesize (WView * view);
 char *mcview_get_ptr_file (WView *, off_t);
 char *mcview_get_ptr_string (WView *, off_t);
-int mcview_get_utf (WView *, off_t, int *, gboolean *);
+gboolean mcview_get_utf (WView * view, off_t byte_index, int *ch, int *ch_len);
 gboolean mcview_get_byte_string (WView *, off_t, int *);
 gboolean mcview_get_byte_none (WView *, off_t, int *);
 void mcview_set_byte (WView *, off_t, byte);

src/viewer/nroff.c
@@ -60,13 +60,12 @@
 static gboolean
 mcview_nroff_get_char (mcview_nroff_t * nroff, int *ret_val, off_t nroff_index)
 {
-    int c;
+    int c = 0;
+
 #ifdef HAVE_CHARSET
     if (nroff->view->utf8)
     {
-        gboolean utf_result;
-        c = mcview_get_utf (nroff->view, nroff_index, &nroff->char_length, &utf_result);
-        if (!utf_result)
+        if (!mcview_get_utf (nroff->view, nroff_index, &c, &nroff->char_length))
         {
             /* we need got symbol in any case */
             nroff->char_length = 1;

commit 5e238fae950cbe3a5cf36aa0393c89b8adee9bc5

Ticket #3783: mcview: fix interpretation of invalid utf-8 symbols.
(mcview_get_utf): report invalid utf-8 as negative integer.

The bug was introduced in 4d65a731c28d53a536a044a85e82223a7aa46bd5.

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

src/viewer/ascii.c
@@ -341,6 +341,8 @@ mcview_char_display (const WView * view, int c, char *s)
  * Normally: stores c, updates state, returns TRUE.
  * At EOF: state is unchanged, c is undefined, returns FALSE.
  *
+ * Just as with mcview_get_utf(), invalid UTF-8 is reported using negative integers.
+ *
  * Also, temporary hack: handle force_max here.
  * TODO: move it to lower layers (datasource.c)?
  */

src/viewer/datasource.c
@@ -147,6 +147,8 @@ mcview_get_ptr_file (WView * view, off_t byte_index)
 
 /* --------------------------------------------------------------------------------------------- */
 
+/* Invalid UTF-8 is reported as negative integers (one for each byte),
+ * see ticket 3783. */
 gboolean
 mcview_get_utf (WView * view, off_t byte_index, int *ch, int *ch_len)
 {
@@ -200,7 +202,8 @@ mcview_get_utf (WView * view, off_t byte_index, int *ch, int *ch_len)
 
     if (res < 0)
     {
-        *ch = (unsigned char) (*str);
+        /* Implicit conversion from signed char to signed int keeps negative values. */
+        *ch = *str;
         *ch_len = 1;
     }
     else