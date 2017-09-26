MidnightCommander/mc

commit 178628e67f5c82e09c07aaf038fc423142fc3310

Ticket #3819: mcview: search doesn't grow file as much as needed.
(mcview_do_search):
  * In main search loop: don't break in case of MC_SEARCH_E_NOTFOUND
error. Make next iteration until buffer can be grown.
  * Make search in the last data chunk.

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

src/viewer/search.c
@@ -366,12 +366,24 @@ mcview_do_search (WView * view, off_t want_search_start)
             break;
         }
 
-        if (view->search->error == MC_SEARCH_E_ABORT || view->search->error == MC_SEARCH_E_NOTFOUND)
+        /* Search error is here.
+         * MC_SEARCH_E_NOTFOUND: continue search
+         * others: stop
+         */
+        if (view->search->error != MC_SEARCH_E_NOTFOUND)
             break;
 
         search_start = growbufsize - view->search->original_len;
     }
     while (search_start > 0 && mcview_may_still_grow (view));
+
+    /* After mcview_may_still_grow (view) == FALSE we have remained last chunk. Search there. */
+    if (!found && view->search->error == MC_SEARCH_E_NOTFOUND && !mcview_search_options.backwards
+        && mcview_find (&vsm, search_start, mcview_get_filesize (view), &match_len))
+    {
+        mcview_search_show_result (view, match_len);
+        found = TRUE;
+    }
 
     status_msg_deinit (STATUS_MSG (&vsm));
 

commit 1221900262dd4a1fcd061b89cc25e7a96f978d5c

Ticket #3855: mcview: search doesn't ask about starting over
Broken in 178628e67f5c82e09c07aaf038fc423142fc3310.

(mcview_do_search): do search in last chunk if data source is the
growing buffer only.

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

src/viewer/search.c
@@ -378,7 +378,8 @@ mcview_do_search (WView * view, off_t want_search_start)
     while (search_start > 0 && mcview_may_still_grow (view));
 
     /* After mcview_may_still_grow (view) == FALSE we have remained last chunk. Search there. */
-    if (!found && view->search->error == MC_SEARCH_E_NOTFOUND && !mcview_search_options.backwards
+    if (view->growbuf_in_use && !found && view->search->error == MC_SEARCH_E_NOTFOUND
+        && !mcview_search_options.backwards
         && mcview_find (&vsm, search_start, mcview_get_filesize (view), &match_len))
     {
         mcview_search_show_result (view, match_len);