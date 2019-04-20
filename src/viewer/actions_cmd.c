7ddd4e9bb49da70839c00e45cea351835e650c0a
Andrew Borodin <aborodin@vmail.ru>
2019-04-20 18:46:37 +0300

    Ticket #3452: mcviewer: implement temporary change of search direction.
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- src/viewer/actions_cmd.c
+++ src/viewer/actions_cmd.c
@@ -455,6 +455,16 @@ mcview_execute_cmd (WView * view, long command)
         mcview_search_options.backwards = TRUE;
         mcview_continue_search_cmd (view);
         break;
+    case CK_SearchOppositeContinue:
+        {
+            gboolean direction;
+
+            direction = mcview_search_options.backwards;
+            mcview_search_options.backwards = !direction;
+            mcview_continue_search_cmd (view);
+            mcview_search_options.backwards = direction;
+        }
+        break;
     case CK_WrapMode:
         /* Toggle between wrapped and unwrapped view */
         mcview_toggle_wrap_mode (view);
