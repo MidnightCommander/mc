MidnightCommander/mc

commit 37ffc024225fd2ef1016babf3cbbeabce4a32899

Ticket #117: refactoring of SIGWINCH hangling.
Use pipe to collect consecutive raised SIGWINCH events.
Then handle multiple events at a time treating them
as a single one.

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

lib/tty/tty.c
@@ -30,6 +30,7 @@
 
 #include <config.h>
 
+#include <errno.h>
 #include <signal.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -160,6 +161,29 @@ tty_got_interrupt (void)
     rv = (got_interrupt != 0);
     got_interrupt = 0;
     return rv;
+}
+
+/* --------------------------------------------------------------------------------------------- */
+
+gboolean
+tty_got_winch (void)
+{
+    gboolean ret = FALSE;
+    ssize_t n;
+
+    /* merge all SIGWINCH events raised to this moment */
+    do
+    {
+        char x[16];
+
+        /* read multiple events at a time  */
+        n = read (sigwinch_pipe[0], &x, sizeof (x));
+        if (n > 0)
+            ret = TRUE;
+    }
+    while (n > 0 || (n == -1 && errno == EINTR));
+
+    return ret;
 }
 
 /* --------------------------------------------------------------------------------------------- */

lib/tty/tty.h
@@ -80,6 +80,8 @@ extern void tty_enable_interrupt_key (void);
 extern void tty_disable_interrupt_key (void);
 extern gboolean tty_got_interrupt (void);
 
+extern gboolean tty_got_winch (void);
+
 extern void tty_reset_prog_mode (void);
 extern void tty_reset_shell_mode (void);
 

lib/widget/dialog-switch.c
@@ -338,7 +338,8 @@ mc_refresh (void)
     if (mc_global.we_are_background)
         return;
 #endif /* ENABLE_BACKGROUND */
-    if (mc_global.tty.winch_flag == 0)
+
+    if (!tty_got_winch ())
         tty_refresh ();
     else
     {
@@ -354,8 +355,6 @@ void
 dialog_change_screen_size (void)
 {
     GList *d;
-
-    mc_global.tty.winch_flag = 0;
 
     tty_change_screen_size ();
 

src/execute.c
@@ -531,8 +531,7 @@ toggle_panels (void)
      * Save sigwinch flag that will be reset in mc_refresh() called via update_panels().
      * There is some problem with screen redraw in ncurses-based mc in this situation.
      */
-    was_sigwinch = mc_global.tty.winch_flag;
-    mc_global.tty.winch_flag = 0;
+    was_sigwinch = tty_got_winch ();
 
 #ifdef ENABLE_SUBSHELL
     if (mc_global.tty.use_subshell)
@@ -555,7 +554,7 @@ toggle_panels (void)
         update_xterm_title_path ();
     }
 
-    if (was_sigwinch != 0 || mc_global.tty.winch_flag != 0)
+    if (was_sigwinch != 0 || tty_got_winch ())
         dialog_change_screen_size ();
     else
         repaint_screen ();

lib/global.c
@@ -104,8 +104,7 @@ mc_global_t mc_global = {
         .disable_colors = FALSE,
         .ugly_line_drawing = FALSE,
         .old_mouse = FALSE,
-        .alternate_plus_minus = FALSE,
-        .winch_flag = 0
+        .alternate_plus_minus = FALSE
     },
 
     .vfs =

lib/global.h
@@ -275,9 +275,6 @@ typedef struct
         /* If true, use + and \ keys normally and select/unselect do if M-+ / M-\.
            and M-- and keypad + / - */
         gboolean alternate_plus_minus;
-
-        /* Set if the window has changed it's size */
-        SIG_ATOMIC_VOLATILE_T winch_flag;
     } tty;
 
     struct

lib/tty/Makefile.am
@@ -16,7 +16,8 @@ TTY_SRC = \
 	color.c color.h \
 	key.c key.h keyxdef.c \
 	mouse.c mouse.h \
-	tty.c tty.h tty-internal.h \
+	tty-internal.c tty-internal.h \
+	tty.c tty.h \
 	win.c win.h
 
 if HAVE_TEXTMODE_X11_SUPPORT

lib/tty/key.c
@@ -2028,7 +2028,7 @@ tty_get_event (struct Gpm_Event *event, gboolean redo_event, gboolean block)
             }
         }
 
-        if (!block || mc_global.tty.winch_flag != 0)
+        if (!block || tty_got_winch ())
         {
             time_addr = &time_out;
             time_out.tv_sec = 0;
@@ -2048,7 +2048,7 @@ tty_get_event (struct Gpm_Event *event, gboolean redo_event, gboolean block)
         {
             if (redo_event)
                 return EV_MOUSE;
-            if (!block || mc_global.tty.winch_flag != 0)
+            if (!block || tty_got_winch ())
                 return EV_NONE;
             vfs_timeout_handler ();
         }

commit 3c4fe21108600ef118c1255e64bf7f340f112435

Ticket #4019: fix SIGWINCH handling,
Fix regression introduced in 37ffc024225fd2ef1016babf3cbbeabce4a32899.

(tty_got_winch): don't read FIFO, just check whether it os empty or not.
It can be called many times without lost of info about SIGWINCH raises.
(tty_flush_winch): read from FIFO to make it empty.

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

lib/tty/tty.c
@@ -35,6 +35,13 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>             /* memset() */
+
+#ifdef HAVE_SYS_SELECT_H
+#include <sys/select.h>
+#else
+#include <sys/time.h>
+#include <sys/types.h>
+#endif
 #include <unistd.h>             /* exit() */
 
 #ifdef HAVE_SYS_IOCTL_H
@@ -168,7 +175,31 @@ tty_got_interrupt (void)
 gboolean
 tty_got_winch (void)
 {
-    gboolean ret = FALSE;
+    fd_set fdset;
+    /* *INDENT-OFF* */
+    /* instant timeout */
+    struct timeval timeout = { .tv_sec = 0, .tv_usec = 0 };
+    /* *INDENT-ON* */
+    int ok;
+
+    FD_ZERO (&fdset);
+    FD_SET (sigwinch_pipe[0], &fdset);
+
+    while ((ok = select (sigwinch_pipe[0] + 1, &fdset, NULL, NULL, &timeout)) < 0)
+        if (errno != EINTR)
+        {
+            perror (_("Cannot check SIGWINCH pipe"));
+            exit (EXIT_FAILURE);
+        }
+
+    return (ok != 0 && FD_ISSET (sigwinch_pipe[0], &fdset));
+}
+
+/* --------------------------------------------------------------------------------------------- */
+
+void
+tty_flush_winch (void)
+{
     ssize_t n;
 
     /* merge all SIGWINCH events raised to this moment */
@@ -178,12 +209,8 @@ tty_got_winch (void)
 
         /* read multiple events at a time  */
         n = read (sigwinch_pipe[0], &x, sizeof (x));
-        if (n > 0)
-            ret = TRUE;
     }
     while (n > 0 || (n == -1 && errno == EINTR));
-
-    return ret;
 }
 
 /* --------------------------------------------------------------------------------------------- */

lib/tty/tty.h
@@ -81,6 +81,7 @@ extern void tty_disable_interrupt_key (void);
 extern gboolean tty_got_interrupt (void);
 
 extern gboolean tty_got_winch (void);
+extern void tty_flush_winch (void);
 
 extern void tty_reset_prog_mode (void);
 extern void tty_reset_shell_mode (void);

lib/widget/dialog-switch.c
@@ -356,6 +356,7 @@ dialog_change_screen_size (void)
 {
     GList *d;
 
+    tty_flush_winch ();
     tty_change_screen_size ();
 
 #ifdef HAVE_SLANG

src/execute.c
@@ -544,6 +544,7 @@ toggle_subshell (void)
      * There is some problem with screen redraw in ncurses-based mc in this situation.
      */
     was_sigwinch = tty_got_winch ();
+    tty_flush_winch ();
 
 #ifdef ENABLE_SUBSHELL
     if (mc_global.tty.use_subshell)