9132211575540f581d230a3b22388c9f842c50c9
Andrew Borodin <aborodin@vmail.ru>
2021-05-29 13:50:30 +0300

    (tty_check_term): clarify description.
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- lib/tty/tty.c
+++ lib/tty/tty.c
@@ -91,8 +91,14 @@ sigintr_handler (int signo)
  *
  * @param force_xterm Set forced the XTerm type
  *
- * @return true if @param force_xterm is true or value of $TERM is one of term*, konsole*
- *              rxvt*, Eterm, dtterm or alacritty
+ * @return true if @param force_xterm is true or value of $TERM is one of following:
+ *         term*
+ *         konsole*
+ *         rxvt*
+ *         Eterm
+ *         dtterm
+ *         alacritty*
+ *         screen* (and DISPLAY environment variable is set).
  */
 gboolean
 tty_check_term (gboolean force_xterm)
