4518885f935ed834b9736a1d4e20f88f7782a259
Andrew Borodin <aborodin@vmail.ru>
2022-02-23 17:40:34 +0300

    (dir_list_load): rename argument: fltr -> filter.
    (dir_list_reload): likewise.
    (handle_dirent): likewise.
    
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- src/filemanager/dir.h
+++ src/filemanager/dir.h
@@ -60,9 +60,9 @@ gboolean dir_list_append (dir_list * list, const char *fname, const struct stat 
                           gboolean link_to_dir, gboolean stale_link);
 
 gboolean dir_list_load (dir_list * list, const vfs_path_t * vpath, GCompareFunc sort,
-                        const dir_sort_options_t * sort_op, const char *fltr);
+                        const dir_sort_options_t * sort_op, const char *filter);
 gboolean dir_list_reload (dir_list * list, const vfs_path_t * vpath, GCompareFunc sort,
-                          const dir_sort_options_t * sort_op, const char *fltr);
+                          const dir_sort_options_t * sort_op, const char *filter);
 void dir_list_sort (dir_list * list, GCompareFunc sort, const dir_sort_options_t * sort_op);
 gboolean dir_list_init (dir_list * list);
 void dir_list_clean (dir_list * list);
