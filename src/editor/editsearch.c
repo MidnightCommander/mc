MidnightCommander/mc

commit 21feb38a8d9161c4004ff4d9d11eb6c010b200e1

mcedit: refactoring of search.
(edit_search_init): new function: initialization of search handler.
(edit_search_deinit): new function: deinitialization of search handler.
(edit_replace_cmd): use edit_search_init().
(edit_search_cmd): likewise.
(editcmd_dialog_search_show): use edit_search_init() and
edit_search_deinit().
(edit_search_options): move definition from editcmd_dialogs.c to editsearch.c.

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

src/editor/editsearch.c
@@ -1,0 +1,96 @@
+/*
+   Search engine of MCEditor.
+
+   Copyright (C) 2021
+   Free Software Foundation, Inc.
+
+   Written by:
+   Andrew Borodin <aborodin@vmail.ru>, 2021
+
+   This file is part of the Midnight Commander.
+
+   The Midnight Commander is free software: you can redistribute it
+   and/or modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation, either version 3 of the License,
+   or (at your option) any later version.
+
+   The Midnight Commander is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <config.h>
+
+#include "lib/global.h"
+#include "lib/search.h"
+#ifdef HAVE_CHARSET
+#include "lib/charsets.h"       /* cp_source */
+#endif
+#include "lib/widget.h"
+
+#include "edit-impl.h"
+#include "editwidget.h"
+
+/*** global variables ****************************************************************************/
+
+edit_search_options_t edit_search_options = {
+    .type = MC_SEARCH_T_NORMAL,
+    .case_sens = FALSE,
+    .backwards = FALSE,
+    .only_in_selection = FALSE,
+    .whole_words = FALSE,
+    .all_codepages = FALSE
+};
+
+/*** file scope macro definitions ****************************************************************/
+
+/*** file scope type declarations ****************************************************************/
+
+/*** file scope variables ************************************************************************/
+
+/* --------------------------------------------------------------------------------------------- */
+/*** file scope functions ************************************************************************/
+/* --------------------------------------------------------------------------------------------- */
+
+/* --------------------------------------------------------------------------------------------- */
+/*** public functions ****************************************************************************/
+/* --------------------------------------------------------------------------------------------- */
+
+gboolean
+edit_search_init (WEdit * edit, const char *str)
+{
+#ifdef HAVE_CHARSET
+    edit->search = mc_search_new (str, cp_source);
+#else
+    edit->search = mc_search_new (str, NULL);
+#endif
+
+    if (edit->search == NULL)
+        return FALSE;
+
+    edit->search->search_type = edit_search_options.type;
+#ifdef HAVE_CHARSET
+    edit->search->is_all_charsets = edit_search_options.all_codepages;
+#endif
+    edit->search->is_case_sensitive = edit_search_options.case_sens;
+    edit->search->whole_words = edit_search_options.whole_words;
+    edit->search->search_fn = edit_search_cmd_callback;
+    edit->search->update_fn = edit_search_update_callback;
+
+    return TRUE;
+}
+
+/* --------------------------------------------------------------------------------------------- */
+
+void
+edit_search_deinit (WEdit * edit)
+{
+    mc_search_free (edit->search);
+    g_free (edit->last_search_string);
+}
+
+/* --------------------------------------------------------------------------------------------- */

src/editor/Makefile.am
@@ -17,6 +17,7 @@ libedit_la_SOURCES = \
 	editdraw.c \
 	editmenu.c \
 	editoptions.c \
+	editsearch.c \
 	editwidget.c editwidget.h \
 	etags.c etags.h \
 	format.c \

src/editor/edit-impl.h
@@ -201,6 +201,9 @@ mc_search_cbret_t edit_search_cmd_callback (const void *user_data, gsize char_of
                                             int *current_char);
 mc_search_cbret_t edit_search_update_callback (const void *user_data, gsize char_offset);
 
+gboolean edit_search_init (WEdit * edit, const char *s);
+void edit_search_deinit (WEdit * edit);
+
 void edit_complete_word_cmd (WEdit * edit);
 void edit_get_match_keyword_cmd (WEdit * edit);
 

src/editor/edit.c
@@ -2204,8 +2204,7 @@ edit_clean (WEdit * edit)
     g_free (edit->redo_stack);
     vfs_path_free (edit->filename_vpath, TRUE);
     vfs_path_free (edit->dir_vpath, TRUE);
-    mc_search_free (edit->search);
-    g_free (edit->last_search_string);
+    edit_search_deinit (edit);
 
 #ifdef HAVE_CHARSET
     if (edit->converter != str_cnv_from_term)

src/editor/editcmd.c
@@ -2672,26 +2672,13 @@ edit_replace_cmd (WEdit * edit, gboolean again)
 
     if (edit->search == NULL)
     {
-#ifdef HAVE_CHARSET
-        edit->search = mc_search_new (input1, cp_source);
-#else
-        edit->search = mc_search_new (input1, NULL);
-#endif
-        if (edit->search == NULL)
+        if (edit_search_init (edit, input1))
+            edit_search_fix_search_start_if_selection (edit);
+        else
         {
             edit->search_start = edit->buffer.curs1;
             goto cleanup;
         }
-        edit->search->search_type = edit_search_options.type;
-#ifdef HAVE_CHARSET
-        edit->search->is_all_charsets = edit_search_options.all_codepages;
-#endif
-        edit->search->is_case_sensitive = edit_search_options.case_sens;
-        edit->search->whole_words = edit_search_options.whole_words;
-        edit->search->search_fn = edit_search_cmd_callback;
-        edit->search->update_fn = edit_search_update_callback;
-        edit->search_line_type = edit_get_search_line_type (edit->search);
-        edit_search_fix_search_start_if_selection (edit);
     }
 
     if (edit->found_len != 0 && edit->search_start == edit->found_start + 1
@@ -2872,9 +2859,6 @@ edit_search_update_callback (const void *user_data, gsize char_offset)
 void
 edit_search_cmd (WEdit * edit, gboolean again)
 {
-    if (edit == NULL)
-        return;
-
     if (!again)
         edit_search (edit);
     else if (edit->last_search_string != NULL)
@@ -2892,30 +2876,14 @@ edit_search_cmd (WEdit * edit, gboolean again)
             history = g_list_first (history);
             g_list_free_full (history, g_free);
 
-#ifdef HAVE_CHARSET
-            edit->search = mc_search_new (edit->last_search_string, cp_source);
-#else
-            edit->search = mc_search_new (edit->last_search_string, NULL);
-#endif
-            if (edit->search == NULL)
+            if (!edit_search_init (edit, edit->last_search_string))
             {
                 /* if not... then ask for an expression */
                 MC_PTR_FREE (edit->last_search_string);
                 edit_search (edit);
             }
             else
-            {
-                edit->search->search_type = edit_search_options.type;
-#ifdef HAVE_CHARSET
-                edit->search->is_all_charsets = edit_search_options.all_codepages;
-#endif
-                edit->search->is_case_sensitive = edit_search_options.case_sens;
-                edit->search->whole_words = edit_search_options.whole_words;
-                edit->search->search_fn = edit_search_cmd_callback;
-                edit->search->update_fn = edit_search_update_callback;
-                edit->search_line_type = edit_get_search_line_type (edit->search);
                 edit_do_search (edit);
-            }
         }
         else
         {

src/editor/editcmd_dialogs.c
@@ -49,15 +49,6 @@
 
 /*** global variables ****************************************************************************/
 
-edit_search_options_t edit_search_options = {
-    .type = MC_SEARCH_T_NORMAL,
-    .case_sens = FALSE,
-    .backwards = FALSE,
-    .only_in_selection = FALSE,
-    .whole_words = FALSE,
-    .all_codepages = FALSE
-};
-
 /*** file scope macro definitions ****************************************************************/
 
 /*** file scope type declarations ****************************************************************/
@@ -177,28 +168,10 @@ editcmd_dialog_search_show (WEdit * edit)
     }
 #endif
 
-    g_free (edit->last_search_string);
+    edit_search_deinit (edit);
     edit->last_search_string = search_text;
-    mc_search_free (edit->search);
 
-#ifdef HAVE_CHARSET
-    edit->search = mc_search_new (edit->last_search_string, cp_source);
-#else
-    edit->search = mc_search_new (edit->last_search_string, NULL);
-#endif
-    if (edit->search != NULL)
-    {
-        edit->search->search_type = edit_search_options.type;
-#ifdef HAVE_CHARSET
-        edit->search->is_all_charsets = edit_search_options.all_codepages;
-#endif
-        edit->search->is_case_sensitive = edit_search_options.case_sens;
-        edit->search->whole_words = edit_search_options.whole_words;
-        edit->search->search_fn = edit_search_cmd_callback;
-        edit->search->update_fn = edit_search_update_callback;
-    }
-
-    return (edit->search != NULL);
+    return edit_search_init (edit, edit->last_search_string);
 }
 
 /* --------------------------------------------------------------------------------------------- */

commit 40e773908c3ff5384a14a58cf653df579925b655

Tickets #4525, #4526: mcedit: fix replacement using regular expressions.
The bug was introduced in 21feb38a8d9161c4004ff4d9d11eb6c010b200e1.

  * (edit_search_init): call edit_get_search_line_type().
  * (edit_search): do mot call edit_get_search_line_type() since it is
  called in edit_search_init().

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

src/editor/editsearch.c
@@ -669,7 +669,6 @@ edit_search (WEdit * edit)
 {
     if (edit_dialog_search_show (edit))
     {
-        edit->search_line_type = edit_get_search_line_type (edit->search);
         edit_search_fix_search_start_if_selection (edit);
         edit_do_search (edit);
     }
@@ -699,6 +698,8 @@ edit_search_init (WEdit * edit, const char *str)
     edit->search->whole_words = edit_search_options.whole_words;
     edit->search->search_fn = edit_search_cmd_callback;
     edit->search->update_fn = edit_search_update_callback;
+
+    edit->search_line_type = edit_get_search_line_type (edit->search);
 
     return TRUE;
 }