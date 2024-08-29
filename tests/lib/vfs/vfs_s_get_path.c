533b152fdcddd141662e6b1bfc98339de890307e
Yury V. Zaytsev <yury@shurup.com>
2024-08-29 16:50:24 +0200

    tests: fix `-Wmissing-variable-declarations` warnings in vfs tests
    ```
    ../../../../tests/lib/vfs/vfs_parse_ls_lga.c:39:23: warning: no previous extern declaration for non-static variable 'test_subclass1' [-Wmissing-variable-declarations]
    struct vfs_s_subclass test_subclass1;
                          ^
    ../../../../tests/lib/vfs/vfs_parse_ls_lga.c:42:21: warning: no previous extern declaration for non-static variable 'vfs_root_entry' [-Wmissing-variable-declarations]
    struct vfs_s_entry *vfs_root_entry;
                        ^
    ../../../../tests/lib/vfs/vfs_s_get_path.c:40:23: warning: no previous extern declaration for non-static variable 'test_subclass1' [-Wmissing-variable-declarations]
    struct vfs_s_subclass test_subclass1, test_subclass2, test_subclass3;
                          ^
    ```
    
    Signed-off-by: Yury V. Zaytsev <yury@shurup.com>

--- tests/lib/vfs/vfs_s_get_path.c
+++ tests/lib/vfs/vfs_s_get_path.c
@@ -37,7 +37,7 @@
 #define ETALON_VFS_NAME "#test2:user:pass@host.net"
 #define ETALON_VFS_URL_NAME "test2://user:pass@host.net"
 
-struct vfs_s_subclass test_subclass1, test_subclass2, test_subclass3;
+static struct vfs_s_subclass test_subclass1, test_subclass2, test_subclass3;
 static struct vfs_class *vfs_test_ops1 = VFS_CLASS (&test_subclass1);
 static struct vfs_class *vfs_test_ops2 = VFS_CLASS (&test_subclass2);
 static struct vfs_class *vfs_test_ops3 = VFS_CLASS (&test_subclass3);
