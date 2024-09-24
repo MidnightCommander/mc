faea164957fdc888a0324eb24f6be3dd9a93354f
Yury V. Zaytsev <yury@shurup.com>
2024-09-24 11:34:14 +0200

    buildsys: add warning for non-GNU gettext and fix shellcheck issues
    Signed-off-by: Yury V. Zaytsev <yury@shurup.com>

--- autogen.sh
+++ autogen.sh
@@ -11,16 +11,20 @@ cd "$srcdir"
 ${AUTORECONF:-autoreconf} --verbose --install --force -I m4 ${AUTORECONF_FLAGS}
 
 # Customize the INSTALL file
-rm -f INSTALL && ln -s doc/INSTALL
+rm -f INSTALL && ln -s doc/INSTALL .
 
 # Generate po/POTFILES.in
-${XGETTEXT:-xgettext} --keyword=_ --keyword=N_ --keyword=Q_ --output=- \
+if ! xgettext -h 2>&1 | grep -e '--keyword=' >/dev/null ; then
+  echo "gettext is unable to extract translations, set XGETTEXT to GNU gettext!" >&2
+else
+  ${XGETTEXT:-xgettext} --keyword=_ --keyword=N_ --keyword=Q_ --output=- \
 	`find . -name '*.[ch]'` | ${SED-sed} -ne '/^#:/{s/#://;s/:[0-9]*/\
 /g;s/ //g;p;}' | \
 	grep -v '^$' | sort | uniq >po/POTFILES.in
+fi
 
-$srcdir/version.sh "$srcdir"
+"$srcdir/version.sh" "$srcdir"
 
-if test -x $srcdir/configure.mc; then
-  $srcdir/configure.mc "$@"
+if test -x "$srcdir/configure.mc"; then
+  "$srcdir/configure.mc" "$@"
 fi
