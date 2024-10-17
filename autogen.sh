1e210762d3a87d130a9e72d2dd46bbf47073e6a9
Yury V. Zaytsev <yury@shurup.com>
2024-10-17 09:15:29 +0200

    buildsys: fix bootstrapped build on Solaris and systems with LC_ALL != C
    Signed-off-by: Yury V. Zaytsev <yury@shurup.com>

--- autogen.sh
+++ autogen.sh
@@ -14,8 +14,9 @@ ${AUTORECONF:-autoreconf} --verbose --install --force -I m4 ${AUTORECONF_FLAGS}
 rm -f INSTALL && ln -s doc/INSTALL .
 
 # Generate po/POTFILES.in
-if ! xgettext -h 2>&1 | grep -e '--keyword=' >/dev/null ; then
+if ! xgettext -h 2>&1 | grep -e '--keyword' >/dev/null ; then
   echo "gettext is unable to extract translations, set XGETTEXT to GNU gettext!" >&2
+  touch po/POTFILES.in
 else
   ${XGETTEXT:-xgettext} --keyword=_ --keyword=N_ --keyword=Q_ --output=- \
 	`find . -name '*.[ch]'` | ${SED-sed} -ne '/^#:/{s/#://;s/:[0-9]*/\
