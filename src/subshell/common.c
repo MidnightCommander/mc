9d4bbe5ee0b22b80061c27631b4945eb80e2ce61
d3m3t3r <46783027+d3m3t3r@users.noreply.github.com>
2024-11-26 18:59:24 +0000

    Support user prompt in fallback precmd emulation

--- src/subshell/common.c
+++ src/subshell/common.c
@@ -1153,18 +1153,19 @@ init_subshell_precmd (char *precmd, size_t buff_size)
      */
     static const char *precmd_fallback =
         " "  /* Useful if the shell supports HISTCONTROL=ignorespace like functionality */
+        "MC_PS1_SAVED=\"$PS1\"; "  /* Save custom PS1 */
         "precmd() { "
             "if [ ! \"${PWD##$HOME}\" ]; then "
                 "MC_PWD=\"~\"; "
             "else "
                 "[ \"${PWD##$HOME/}\" = \"$PWD\" ] && MC_PWD=\"$PWD\" || MC_PWD=\"~/${PWD##$HOME/}\"; "
             "fi; "
-            "echo \"$USER@$(hostname -s):$MC_PWD\"; "
+            "echo \"${MC_PS1_SAVED:-$USER@$(hostname -s):$MC_PWD\\$ }\"; "
             "pwd>&%d; "
             "kill -STOP $$; "
         "}; "
         "PRECMD=precmd; "
-        "PS1='$($PRECMD)$ '\n";
+        "PS1='$($PRECMD)'\n";
 
     switch (mc_global.shell->type)
     {
