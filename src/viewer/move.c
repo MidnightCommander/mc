a83a6f4acf842b4a3d8406e16e4c42545eebd7ab
Andrew Borodin <aborodin@vmail.ru>
2024-12-14 09:59:09 +0300

    (mcview_moveto_bottom): fix search start and stop positions.
    After move to the end of file, set the start positions to EOL
    to make backward search starting directly from EOL.
    
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- src/viewer/move.c
+++ src/viewer/move.c
@@ -263,6 +263,9 @@ mcview_moveto_bottom (WView *view)
         view->dpy_paragraph_skip_lines = 0;
         view->dpy_wrap_dirty = TRUE;
         mcview_move_up (view, view->data_area.lines);
+        /* start backward search from EOF */
+        view->search_start = filesize;
+        view->search_end = view->search_start;
     }
 }
 
