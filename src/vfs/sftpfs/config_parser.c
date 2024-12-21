MidnightCommander/mc

commit 4c998ac636eb3cfad0ec29bba92c1548fdbeb977

sftpfs: expand %h
Properly parse %h hostname token, which is widely used in HostName directive within
~/.ssh/config:

Host testing*
  HostName %h.eu.example.com

Host staging*
  HostName %h.us.example.net

# this host will be expanded to `testing-1.eu.example.com`
% mc sftp://username@testing-1

Closes MidnightCommander/mc#197.

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

src/vfs/sftpfs/config_parser.c
@@ -131,6 +131,24 @@ sftpfs_correct_file_name (const char *filename)
 }
 
 /* --------------------------------------------------------------------------------------------- */
+/**
+ * Try to expand remote hostname (%h) token.
+ *
+ * @param host hostname string provided by user
+ * @param real_host real hostname string retrieved from the corresponding HostName directive
+ * @return newly allocated string with possibly expanded hostname
+ */
+
+static char *
+sftpsfs_expand_hostname (const char *host, const char *real_host)
+{
+    if (g_str_has_prefix (real_host, "%h"))
+        return g_strconcat (host, real_host + 2, (char *) NULL);
+
+    return g_strdup (real_host);
+}
+
+/* --------------------------------------------------------------------------------------------- */
 
 #define POINTER_TO_STRUCTURE_MEMBER(type)  \
     ((type) ((char *) config_entity + (size_t) config_variables[i].offset))
@@ -376,7 +394,8 @@ sftpfs_fill_connection_data_from_config (struct vfs_s_super *super, GError ** mc
     if (config_entity->real_host != NULL)
     {
         g_free (super->path_element->host);
-        super->path_element->host = g_strdup (config_entity->real_host);
+        super->path_element->host =
+            sftpsfs_expand_hostname (super->path_element->host, config_entity->real_host);
     }
 
     if (config_entity->identity_file != NULL)

commit d0d8ae90eb01d1c3d86b8cd68e4df77352a4f8bc

Ticket #4620: (sftpfs_fill_connection_data_from_config): fix use-after-free.
Fix Use-after-free in sftpfs_fill_connection_data_from_config()

Found by Clang-19 Static Analyzer

The bug was introduced in 4c998ac636eb3cfad0ec29bba92c1548fdbeb977.

Reported-by: Andreas Mohr <and@gmx.li>
Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

src/vfs/sftpfs/config_parser.c
@@ -393,9 +393,12 @@ sftpfs_fill_connection_data_from_config (struct vfs_s_super *super, GError **mce
 
     if (config_entity->real_host != NULL)
     {
-        g_free (super->path_element->host);
+        char *tmp_str = super->path_element->host;
+
         super->path_element->host =
             sftpsfs_expand_hostname (super->path_element->host, config_entity->real_host);
+
+        g_free (tmp_str);
     }
 
     if (config_entity->identity_file != NULL)