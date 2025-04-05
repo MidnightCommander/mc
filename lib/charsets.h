9bc6916715ae3f0b4928f147e4f5e531e8e912c1
Andrew Borodin <aborodin@vmail.ru>
2025-04-05 15:15:19 +0300

    lib/charsets.[ch]: cleanup.
      * (init_translation_table): fix typos.
      * (convert_string): function isn't implemented, remove delaration.
      * (convert_to_display): function is unused, remove.
      * (convert_from_input): likewise.
    
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- lib/charsets.h
+++ lib/charsets.h
@@ -34,9 +34,6 @@ void load_codepages_list (void);
 void free_codepages_list (void);
 gboolean is_supported_encoding (const char *encoding);
 char *init_translation_table (int cpsource, int cpdisplay);
-void convert_to_display (char *str);
-void convert_from_input (char *str);
-void convert_string (unsigned char *str);
 
 /*
  * Converter from utf to selected codepage
