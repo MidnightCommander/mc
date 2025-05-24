MidnightCommander/mc

commit f80dc2ba2b8bb635447fadc77b3e06937aa13017

Ticket #4637: panels not refreshed after editing file with internal editor.
(dialog_switch_process_pending): reload panels forced after closing
internal editor/viewer/diffviewer if screen size was not changed.

Fixes 213119c8da268911f53990b587de3751dfcc006a.

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

lib/widget/dialog-switch.c
@@ -266,28 +266,35 @@ dialog_switch_list (void)
 int
 dialog_switch_process_pending (void)
 {
+    WDialog *h = DIALOG (mc_current->data);
     int ret = 0;
 
-    while (dialog_switch_pending)
+    if (!dialog_switch_pending)
     {
-        WDialog *h = DIALOG (mc_current->data);
-        Widget *wh = WIDGET (h);
-
-        dialog_switch_pending = FALSE;
-        widget_set_state (wh, WST_SUSPENDED, TRUE);
-        ret = dlg_run (h);
-        if (widget_get_state (wh, WST_CLOSED))
+        // return to panels and reload them forced
+        if (mc_global.mc_run_mode == MC_RUN_FULL && h == filemanager)
+            mc_event_raise (MCEVENT_GROUP_FILEMANAGER, "update_panels", NULL);
+    }
+    else
+        while (dialog_switch_pending)
         {
-            widget_destroy (wh);
+            Widget *wh = WIDGET (h);
 
-            // return to panels
-            if (mc_global.mc_run_mode == MC_RUN_FULL)
+            dialog_switch_pending = FALSE;
+            widget_set_state (wh, WST_SUSPENDED, TRUE);
+            ret = dlg_run (h);
+            if (widget_get_state (wh, WST_CLOSED))
             {
-                mc_current = g_list_find (mc_dialogs, filemanager);
-                mc_event_raise (MCEVENT_GROUP_FILEMANAGER, "update_panels", NULL);
+                widget_destroy (wh);
+
+                // return to panels
+                if (mc_global.mc_run_mode == MC_RUN_FULL)
+                {
+                    mc_current = g_list_find (mc_dialogs, filemanager);
+                    mc_event_raise (MCEVENT_GROUP_FILEMANAGER, "update_panels", NULL);
+                }
             }
         }
-    }
 
     repaint_screen ();
 

commit 3fe40d7aec3b08fdebaccb347336c344396467cc

Ticket #4712: fix segfault at mcview exit.
How to reproduce:
  * Run mcview some_file or mc -v some_file
  * Exit (double Escape or F10)
Actual behavior: segmentation fault.
Expected behavior: normal exit.

The bug was introduced in f80dc2ba2b8bb635447fadc77b3e06937aa13017.

  * (dialog_switch_list): fix NULL dereference: exit immediately if
    dialog list is empty.

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

lib/widget/dialog-switch.c
@@ -266,8 +266,13 @@ dialog_switch_list (void)
 int
 dialog_switch_process_pending (void)
 {
-    WDialog *h = DIALOG (mc_current->data);
+    WDialog *h;
     int ret = 0;
+
+    if (mc_current == NULL)
+        return ret;
+
+    h = DIALOG (mc_current->data);
 
     if (!dialog_switch_pending)
     {