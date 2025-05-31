MidnightCommander/mc

commit 1ac839d20c2a277d54aa00286b69f61ffeaec929

Ticket #4600: fix segfault on panel filter.
mc segfaults if filter makes file panel empty.

  * (panel_current_entry): improve. Add checks if panel->current is in
  range of file list.
  * A lot of changes to use modified panel_current_entry().
  * (format_file):
  * (display_mini_info): fix drawing of mini-status if file panel is
  empty.

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

src/filemanager/filemanager.c
@@ -150,9 +150,14 @@ stop_dialogs (void)
 static void
 treebox_cmd (void)
 {
+    const file_entry_t *fe;
     char *sel_dir;
 
-    sel_dir = tree_box (panel_current_entry (current_panel)->fname->str);
+    fe = panel_current_entry (current_panel);
+    if (fe == NULL)
+        return;
+
+    sel_dir = tree_box (fe->fname->str);
     if (sel_dir != NULL)
     {
         vfs_path_t *sel_vdir;
@@ -720,7 +725,7 @@ put_link (WPanel *panel)
 
     fe = panel_current_entry (panel);
 
-    if (S_ISLNK (fe->st.st_mode))
+    if (fe != NULL && S_ISLNK (fe->st.st_mode))
     {
         char buffer[MC_MAXPATHLEN];
         vfs_path_t *vpath;
@@ -761,8 +766,6 @@ put_other_link (void)
 static void
 put_current_selected (void)
 {
-    const char *tmp;
-
     if (!command_prompt)
         return;
 
@@ -773,12 +776,16 @@ put_current_selected (void)
 
         tree = (WTree *) get_panel_widget (get_current_index ());
         selected_name = tree_selected_name (tree);
-        tmp = vfs_path_as_str (selected_name);
+        command_insert (cmdline, vfs_path_as_str (selected_name), TRUE);
     }
     else
-        tmp = panel_current_entry (current_panel)->fname->str;
+    {
+        const file_entry_t *fe;
 
-    command_insert (cmdline, tmp, TRUE);
+        fe = panel_current_entry (current_panel);
+        if (fe != NULL)
+            command_insert (cmdline, fe->fname->str, TRUE);
+    }
 }
 
 /* --------------------------------------------------------------------------------------------- */
@@ -792,7 +799,13 @@ put_tagged (WPanel *panel)
     input_disable_update (cmdline);
 
     if (panel->marked == 0)
-        command_insert (cmdline, panel_current_entry (panel)->fname->str, TRUE);
+    {
+        const file_entry_t *fe;
+
+        fe = panel_current_entry (current_panel);
+        if (fe != NULL)
+            command_insert (cmdline, fe->fname->str, TRUE);
+    }
     else
     {
         int i;

src/diffviewer/ydiff.c
@@ -3460,6 +3460,12 @@ dview_diff_cmd (const void *f0, const void *f1)
             const file_entry_t *fe0, *fe1;
 
             fe0 = panel_current_entry (panel0);
+            if (fe0 == NULL)
+            {
+                message (D_ERROR, MSG_ERROR, "%s", _("File name is empty!"));
+                goto ret;
+            }
+
             file0 = vfs_path_append_new (panel0->cwd_vpath, fe0->fname->str, (char *) NULL);
             is_dir0 = S_ISDIR (fe0->st.st_mode);
             if (is_dir0)
@@ -3470,6 +3476,11 @@ dview_diff_cmd (const void *f0, const void *f1)
             }
 
             fe1 = panel_current_entry (panel1);
+            if (fe1 == NULL)
+            {
+                message (D_ERROR, MSG_ERROR, "%s", _("File name is empty!"));
+                goto ret;
+            }
             file1 = vfs_path_append_new (panel1->cwd_vpath, fe1->fname->str, (char *) NULL);
             is_dir1 = S_ISDIR (fe1->st.st_mode);
             if (is_dir1)

src/filemanager/achown.c
@@ -1020,7 +1020,16 @@ advanced_chown_cmd (WPanel *panel)
         if (panel->marked != 0)
             fname = next_file (panel);  /* next marked file */
         else
-            fname = panel_current_entry (panel)->fname; /* single file */
+        {
+            /* single file */
+            const file_entry_t *fe;
+
+            fe = panel_current_entry (panel);
+            if (fe == NULL)
+                break;
+
+            fname = fe->fname;
+        }
 
         vpath = vfs_path_from_str (fname->str);
 

src/filemanager/chattr.c
@@ -1227,7 +1227,16 @@ chattr_cmd (WPanel *panel)
         if (panel->marked != 0)
             fname = next_file (panel);  /* next marked file */
         else
-            fname = panel_current_entry (panel)->fname; /* single file */
+        {
+            /* single file */
+            const file_entry_t *fe;
+
+            fe = panel_current_entry (panel);
+            if (fe == NULL)
+                break;
+
+            fname = fe->fname;
+        }
 
         vpath = vfs_path_from_str (fname->str);
 

src/filemanager/chmod.c
@@ -552,7 +552,16 @@ chmod_cmd (WPanel *panel)
         if (panel->marked != 0)
             fname = next_file (panel);  /* next marked file */
         else
-            fname = panel_current_entry (panel)->fname; /* single file */
+        {
+            /* single file */
+            const file_entry_t *fe;
+
+            fe = panel_current_entry (panel);
+            if (fe == NULL)
+                break;
+
+            fname = fe->fname;
+        }
 
         vpath = vfs_path_from_str (fname->str);
 

src/filemanager/chown.c
@@ -421,7 +421,16 @@ chown_cmd (WPanel *panel)
         if (panel->marked != 0)
             fname = next_file (panel);  /* next marked file */
         else
-            fname = panel_current_entry (panel)->fname; /* single file */
+        {
+            /* single file */
+            const file_entry_t *fe;
+
+            fe = panel_current_entry (panel);
+            if (fe == NULL)
+                break;
+
+            fname = fe->fname;
+        }
 
         vpath = vfs_path_from_str (fname->str);
 

src/filemanager/cmd.c
@@ -125,6 +125,8 @@ do_view_cmd (WPanel *panel, gboolean plain_view)
     const file_entry_t *fe;
 
     fe = panel_current_entry (panel);
+    if (fe == NULL)
+        return;
 
     /* Directories are viewed by changing to them */
     if (S_ISDIR (fe->st.st_mode) || link_isdir (fe))
@@ -576,12 +578,16 @@ view_cmd (WPanel *panel)
 void
 view_file_cmd (const WPanel *panel)
 {
+    const file_entry_t *fe;
     char *filename;
     vfs_path_t *vpath;
 
+    fe = panel_current_entry (panel);
+    if (fe == NULL)
+        return;
+
     filename =
-        input_expand_dialog (_("View file"), _("Filename:"),
-                             MC_HISTORY_FM_VIEW_FILE, panel_current_entry (panel)->fname->str,
+        input_expand_dialog (_("View file"), _("Filename:"), MC_HISTORY_FM_VIEW_FILE, fe->fname->str,
                              INPUT_COMPLETE_FILENAMES);
     if (filename == NULL)
         return;
@@ -609,7 +615,15 @@ view_filtered_cmd (const WPanel *panel)
     const char *initial_command;
 
     if (input_is_empty (cmdline))
-        initial_command = panel_current_entry (panel)->fname->str;
+    {
+        const file_entry_t *fe;
+
+        fe = panel_current_entry (panel);
+        if (fe == NULL)
+            return;
+
+        initial_command = fe->fname->str;
+    }
     else
         initial_command = input_get_ctext (cmdline);
 
@@ -673,9 +687,14 @@ edit_file_at_line (const vfs_path_t *what_vpath, gboolean internal, long start_l
 void
 edit_cmd (const WPanel *panel)
 {
+    const file_entry_t *fe;
     vfs_path_t *fname;
 
-    fname = vfs_path_from_str (panel_current_entry (panel)->fname->str);
+    fe = panel_current_entry (panel);
+    if (fe == NULL)
+        return;
+
+    fname = vfs_path_from_str (fe->fname->str);
     if (regex_command (fname, "Edit") == 0)
         do_edit (fname);
     vfs_path_free (fname, TRUE);
@@ -687,9 +706,14 @@ edit_cmd (const WPanel *panel)
 void
 edit_cmd_force_internal (const WPanel *panel)
 {
+    const file_entry_t *fe;
     vfs_path_t *fname;
 
-    fname = vfs_path_from_str (panel_current_entry (panel)->fname->str);
+    fe = panel_current_entry (panel);
+    if (fe == NULL)
+        return;
+
+    fname = vfs_path_from_str (fe->fname->str);
     if (regex_command (fname, "Edit") == 0)
         edit_file_at_line (fname, TRUE, 1);
     vfs_path_free (fname, TRUE);
@@ -736,6 +760,8 @@ mkdir_cmd (WPanel *panel)
     const char *name = "";
 
     fe = panel_current_entry (panel);
+    if (fe == NULL)
+        return;
 
     /* If 'on' then automatically fills name with current item name */
     if (auto_fill_mkdir_name && !DIR_IS_DOTDOT (fe->fname->str))
@@ -1055,11 +1081,11 @@ swap_cmd (void)
 void
 link_cmd (link_type_t link_type)
 {
-    const char *filename;
+    const file_entry_t *fe;
 
-    filename = panel_current_entry (current_panel)->fname->str;
-    if (filename != NULL)
-        do_link (link_type, filename);
+    fe = panel_current_entry (current_panel);
+    if (fe != NULL)
+        do_link (link_type, fe->fname->str);
 }
 
 /* --------------------------------------------------------------------------------------------- */
@@ -1071,6 +1097,9 @@ edit_symlink_cmd (void)
     const char *p;
 
     fe = panel_current_entry (current_panel);
+    if (fe == NULL)
+        return;
+
     p = fe->fname->str;
 
     if (!S_ISLNK (fe->st.st_mode))
@@ -1218,7 +1247,8 @@ smart_dirsize_cmd (WPanel *panel)
     const file_entry_t *entry;
 
     entry = panel_current_entry (panel);
-    if ((S_ISDIR (entry->st.st_mode) && DIR_IS_DOTDOT (entry->fname->str)) || panel->dirs_marked)
+    if ((entry != NULL && S_ISDIR (entry->st.st_mode) && DIR_IS_DOTDOT (entry->fname->str))
+        || panel->dirs_marked)
         dirsizes_cmd (panel);
     else
         single_dirsize_cmd (panel);
@@ -1233,7 +1263,7 @@ single_dirsize_cmd (WPanel *panel)
 
     entry = panel_current_entry (panel);
 
-    if (S_ISDIR (entry->st.st_mode) && !DIR_IS_DOTDOT (entry->fname->str))
+    if (entry != NULL && S_ISDIR (entry->st.st_mode) && !DIR_IS_DOTDOT (entry->fname->str))
     {
         size_t dir_count = 0;
         size_t count = 0;

src/filemanager/file.c
@@ -1751,6 +1751,8 @@ do_move_dir_dir (const WPanel *panel, file_op_total_context_t *tctx, file_op_con
 static const char *
 panel_get_file (const WPanel *panel)
 {
+    const file_entry_t *fe;
+
     if (get_current_type () == view_tree)
     {
         WTree *tree;
@@ -1770,7 +1772,9 @@ panel_get_file (const WPanel *panel)
                 return panel->dir.list[i].fname->str;
     }
 
-    return panel_current_entry (panel)->fname->str;
+    fe = panel_current_entry (panel);
+
+    return (fe == NULL ? NULL : fe->fname->str);
 }
 
 /* --------------------------------------------------------------------------------------------- */
@@ -1782,9 +1786,17 @@ check_single_entry (const WPanel *panel, gboolean force_single, struct stat *src
     gboolean ok;
 
     if (force_single)
-        source = panel_current_entry (panel)->fname->str;
+    {
+        const file_entry_t *fe;
+
+        fe = panel_current_entry (panel);
+        source = fe == NULL ? NULL : fe->fname->str;
+    }
     else
         source = panel_get_file (panel);
+
+    if (source == NULL)
+        return NULL;
 
     ok = !DIR_IS_DOTDOT (source);
 
@@ -3529,9 +3541,12 @@ panel_operate (void *source_panel, FileOperation operation, gboolean force_singl
     else
 #endif /* ENABLE_BACKGROUND */
     {
+        const file_entry_t *fe;
+
         if (operation == OP_DELETE)
             dialog_type = FILEGUI_DIALOG_DELETE_ITEM;
-        else if (single_entry && S_ISDIR (panel_current_entry (panel)->st.st_mode))
+        else if (single_entry
+                 && ((fe = panel_current_entry (panel)) == NULL ? FALSE : S_ISDIR (fe->st.st_mode)))
             dialog_type = FILEGUI_DIALOG_MULTI_ITEM;
         else if (single_entry || force_single)
             dialog_type = FILEGUI_DIALOG_ONE_ITEM;

commit a47954a552bc8735640178dc977f61fd897c0d3c

Ticket #4716: (put_tagged): put file from other panel to command line.
Bug was introduced in 1ac839d20c2a277d54aa00286b69f61ffeaec929.

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

src/filemanager/filemanager.c
@@ -784,7 +784,7 @@ put_tagged (WPanel *panel)
     {
         const file_entry_t *fe;
 
-        fe = panel_current_entry (current_panel);
+        fe = panel_current_entry (panel);
         if (fe != NULL)
             command_insert (cmdline, fe->fname->str, TRUE);
     }