17c705d1f40cb90e3854cc1c442741f4177bd63b
Egmont Koblinger <egmont@gmail.com>
2025-10-24 01:03:03 +0200

    Ticket #3158, #4799: Clean up and fix frame drawing
    Fix the ncurses build to show double frame characters in UTF-8 mode
    wherever the skin requests that.
    
    Fall back to single frame characters, rather than displaying random
    letters, if the value specified in the skin file cannot be converted to
    the current locale.
    
    Internal refactoring and cleanup: Use constants that are independent from
    the screen drawing library. Separate the roles (e.g. "the character to be
    placed in the upper left corner of a supposedly doubly-framed dialog")
    from the actual characters (e.g. "double upper left corner line").
    
    Minor regression due to the refactoring: Double lines are replaced by
    single ones in slang build under KOI8-R locale. It's probably not worth it
    to fix this.
    
    Signed-off-by: Egmont Koblinger <egmont@gmail.com>

--- lib/skin/lines.c
+++ lib/skin/lines.c
@@ -29,6 +29,7 @@
 
 #include "internal.h"
 #include "lib/tty/tty.h"
+#include "lib/strutil.h"
 
 /*** global variables ****************************************************************************/
 
@@ -44,17 +45,54 @@
 /*** file scope functions ************************************************************************/
 /* --------------------------------------------------------------------------------------------- */
 
-static int
-mc_skin_lines_load_frm (mc_skin_t *mc_skin, const char *name)
+static mc_tty_char_t
+mc_skin_lines_get_tty_char (mc_skin_t *mc_skin, const char *name, mc_tty_char_t def)
 {
-    int ret;
-    char *frm_val;
+    GIConv conv;
+    GString *buffer;
+    estr_t conv_res;
+    char *value_utf8;
 
-    frm_val = mc_config_get_string_raw (mc_skin->config, "Lines", name, " ");
-    ret = mc_tty_normalize_lines_char (frm_val);
-    g_free (frm_val);
+    value_utf8 = mc_config_get_string_raw (mc_skin->config, "Lines", name, "");
+    const gunichar c = g_utf8_get_char_validated (value_utf8, -1);
+    if (c == (gunichar) (-1) || c == (gunichar) (-2))
+    {
+        g_free (value_utf8);
+        return def;
+    }
 
-    return ret;
+    // Prefer MC_ACS_* values, even if there's a codepoint for this character in the current locale.
+    // This gives more flexibility to the ncurses or slang implementation in handling them, and also
+    // prevents us from hitting an ncurses bug (mc ticket #4799#issuecomment-3442057420).
+    const mc_tty_char_t mc_acs = tty_unicode_to_mc_acs (c);
+    if (mc_acs != 0)
+    {
+        g_free (value_utf8);
+        return mc_acs;
+    }
+
+    // nothing left to do in UTF-8 locales
+    if (mc_global.utf8_display)
+    {
+        g_free (value_utf8);
+        return c;
+    }
+
+    // convert to 8-bit charset
+    conv = str_crt_conv_from ("UTF-8");
+    if (conv == INVALID_CONV)
+    {
+        g_free (value_utf8);
+        return def;
+    }
+
+    buffer = g_string_new ("");
+    conv_res = str_convert (conv, value_utf8, buffer);
+    const mc_tty_char_t retval = (conv_res == ESTR_SUCCESS) ? (unsigned char) buffer->str[0] : def;
+    str_close_conv (conv);
+    g_string_free (buffer, TRUE);
+    g_free (value_utf8);
+    return retval;
 }
 
 /* --------------------------------------------------------------------------------------------- */
@@ -70,29 +108,46 @@ mc_skin_lines_parse_ini_file (mc_skin_t *mc_skin)
         mc_skin_hardcoded_ugly_lines (mc_skin);
 
     // single lines
-    mc_tty_frm[MC_TTY_FRM_VERT] = mc_skin_lines_load_frm (mc_skin, "vert");
-    mc_tty_frm[MC_TTY_FRM_HORIZ] = mc_skin_lines_load_frm (mc_skin, "horiz");
-    mc_tty_frm[MC_TTY_FRM_LEFTTOP] = mc_skin_lines_load_frm (mc_skin, "lefttop");
-    mc_tty_frm[MC_TTY_FRM_RIGHTTOP] = mc_skin_lines_load_frm (mc_skin, "righttop");
-    mc_tty_frm[MC_TTY_FRM_LEFTBOTTOM] = mc_skin_lines_load_frm (mc_skin, "leftbottom");
-    mc_tty_frm[MC_TTY_FRM_RIGHTBOTTOM] = mc_skin_lines_load_frm (mc_skin, "rightbottom");
-    mc_tty_frm[MC_TTY_FRM_TOPMIDDLE] = mc_skin_lines_load_frm (mc_skin, "topmiddle");
-    mc_tty_frm[MC_TTY_FRM_BOTTOMMIDDLE] = mc_skin_lines_load_frm (mc_skin, "bottommiddle");
-    mc_tty_frm[MC_TTY_FRM_LEFTMIDDLE] = mc_skin_lines_load_frm (mc_skin, "leftmiddle");
-    mc_tty_frm[MC_TTY_FRM_RIGHTMIDDLE] = mc_skin_lines_load_frm (mc_skin, "rightmiddle");
-    mc_tty_frm[MC_TTY_FRM_CROSS] = mc_skin_lines_load_frm (mc_skin, "cross");
+    mc_tty_frm[MC_TTY_FRM_HORIZ] = mc_skin_lines_get_tty_char (mc_skin, "horiz", MC_ACS_HLINE);
+    mc_tty_frm[MC_TTY_FRM_VERT] = mc_skin_lines_get_tty_char (mc_skin, "vert", MC_ACS_VLINE);
+    mc_tty_frm[MC_TTY_FRM_LEFTTOP] =
+        mc_skin_lines_get_tty_char (mc_skin, "lefttop", MC_ACS_ULCORNER);
+    mc_tty_frm[MC_TTY_FRM_RIGHTTOP] =
+        mc_skin_lines_get_tty_char (mc_skin, "righttop", MC_ACS_URCORNER);
+    mc_tty_frm[MC_TTY_FRM_LEFTBOTTOM] =
+        mc_skin_lines_get_tty_char (mc_skin, "leftbottom", MC_ACS_LLCORNER);
+    mc_tty_frm[MC_TTY_FRM_RIGHTBOTTOM] =
+        mc_skin_lines_get_tty_char (mc_skin, "rightbottom", MC_ACS_LRCORNER);
+    mc_tty_frm[MC_TTY_FRM_TOPMIDDLE] =
+        mc_skin_lines_get_tty_char (mc_skin, "topmiddle", MC_ACS_TTEE);
+    mc_tty_frm[MC_TTY_FRM_BOTTOMMIDDLE] =
+        mc_skin_lines_get_tty_char (mc_skin, "bottommiddle", MC_ACS_BTEE);
+    mc_tty_frm[MC_TTY_FRM_LEFTMIDDLE] =
+        mc_skin_lines_get_tty_char (mc_skin, "leftmiddle", MC_ACS_LTEE);
+    mc_tty_frm[MC_TTY_FRM_RIGHTMIDDLE] =
+        mc_skin_lines_get_tty_char (mc_skin, "rightmiddle", MC_ACS_RTEE);
+    mc_tty_frm[MC_TTY_FRM_CROSS] = mc_skin_lines_get_tty_char (mc_skin, "cross", MC_ACS_PLUS);
 
     // double lines
-    mc_tty_frm[MC_TTY_FRM_DVERT] = mc_skin_lines_load_frm (mc_skin, "dvert");
-    mc_tty_frm[MC_TTY_FRM_DHORIZ] = mc_skin_lines_load_frm (mc_skin, "dhoriz");
-    mc_tty_frm[MC_TTY_FRM_DLEFTTOP] = mc_skin_lines_load_frm (mc_skin, "dlefttop");
-    mc_tty_frm[MC_TTY_FRM_DRIGHTTOP] = mc_skin_lines_load_frm (mc_skin, "drighttop");
-    mc_tty_frm[MC_TTY_FRM_DLEFTBOTTOM] = mc_skin_lines_load_frm (mc_skin, "dleftbottom");
-    mc_tty_frm[MC_TTY_FRM_DRIGHTBOTTOM] = mc_skin_lines_load_frm (mc_skin, "drightbottom");
-    mc_tty_frm[MC_TTY_FRM_DTOPMIDDLE] = mc_skin_lines_load_frm (mc_skin, "dtopmiddle");
-    mc_tty_frm[MC_TTY_FRM_DBOTTOMMIDDLE] = mc_skin_lines_load_frm (mc_skin, "dbottommiddle");
-    mc_tty_frm[MC_TTY_FRM_DLEFTMIDDLE] = mc_skin_lines_load_frm (mc_skin, "dleftmiddle");
-    mc_tty_frm[MC_TTY_FRM_DRIGHTMIDDLE] = mc_skin_lines_load_frm (mc_skin, "drightmiddle");
+    mc_tty_frm[MC_TTY_FRM_DHORIZ] =
+        mc_skin_lines_get_tty_char (mc_skin, "dhoriz", MC_ACS_DBL_HLINE);
+    mc_tty_frm[MC_TTY_FRM_DVERT] = mc_skin_lines_get_tty_char (mc_skin, "dvert", MC_ACS_DBL_VLINE);
+    mc_tty_frm[MC_TTY_FRM_DLEFTTOP] =
+        mc_skin_lines_get_tty_char (mc_skin, "dlefttop", MC_ACS_DBL_ULCORNER);
+    mc_tty_frm[MC_TTY_FRM_DRIGHTTOP] =
+        mc_skin_lines_get_tty_char (mc_skin, "drighttop", MC_ACS_DBL_URCORNER);
+    mc_tty_frm[MC_TTY_FRM_DLEFTBOTTOM] =
+        mc_skin_lines_get_tty_char (mc_skin, "dleftbottom", MC_ACS_DBL_LLCORNER);
+    mc_tty_frm[MC_TTY_FRM_DRIGHTBOTTOM] =
+        mc_skin_lines_get_tty_char (mc_skin, "drightbottom", MC_ACS_DBL_LRCORNER);
+    mc_tty_frm[MC_TTY_FRM_DTOPMIDDLE] =
+        mc_skin_lines_get_tty_char (mc_skin, "dtopmiddle", MC_ACS_DBL_TTEE);
+    mc_tty_frm[MC_TTY_FRM_DBOTTOMMIDDLE] =
+        mc_skin_lines_get_tty_char (mc_skin, "dbottommiddle", MC_ACS_DBL_BTEE);
+    mc_tty_frm[MC_TTY_FRM_DLEFTMIDDLE] =
+        mc_skin_lines_get_tty_char (mc_skin, "dleftmiddle", MC_ACS_DBL_LTEE);
+    mc_tty_frm[MC_TTY_FRM_DRIGHTMIDDLE] =
+        mc_skin_lines_get_tty_char (mc_skin, "drightmiddle", MC_ACS_DBL_RTEE);
 }
 
 /* --------------------------------------------------------------------------------------------- */
--- lib/tty/tty.h
+++ lib/tty/tty.h
@@ -25,13 +25,51 @@
 #define KEY_KP_SUBTRACT 4002
 #define KEY_KP_MULTIPLY 4003
 
+// For frame characters (single or double) it's one of the MC_ACS_* special values.
+// Otherwise the character code in the locale's encoding.
+typedef unsigned int mc_tty_char_t;
+
 /*** enums ***************************************************************************************/
 
+// These values always represent the given line drawing characters, in a common way regardless of
+// the underlying screen library.
+// These values really have the type 'mc_tty_char_t', but enum cannot denote it.
+enum
+{
+    // Start the numbers above the highest Unicode value.
+    MC_ACS_HLINE = 0x110000,  // ─
+    MC_ACS_VLINE,             // │
+    MC_ACS_ULCORNER,          // ┌
+    MC_ACS_URCORNER,          // ┐
+    MC_ACS_LLCORNER,          // └
+    MC_ACS_LRCORNER,          // ┘
+    MC_ACS_TTEE,              // ┬
+    MC_ACS_BTEE,              // ┴
+    MC_ACS_LTEE,              // ├
+    MC_ACS_RTEE,              // ┤
+    MC_ACS_PLUS,              // ┼
+
+    // These don't have ACS_ counterparts in ncurses or slang, these aren't available as DEC line
+    // drawing chars.
+    MC_ACS_DBL_HLINE,     // ═
+    MC_ACS_DBL_VLINE,     // ║
+    MC_ACS_DBL_ULCORNER,  // ╔
+    MC_ACS_DBL_URCORNER,  // ╗
+    MC_ACS_DBL_LLCORNER,  // ╚
+    MC_ACS_DBL_LRCORNER,  // ╝
+    MC_ACS_DBL_TTEE,      // ╤
+    MC_ACS_DBL_BTEE,      // ╧
+    MC_ACS_DBL_LTEE,      // ╟
+    MC_ACS_DBL_RTEE,      // ╢
+};
+
+// These refer to the roles, the given positions of more prominent and less prominent boxes.
+// The actual characters, taken from the skin, may or may not match the role name.
 typedef enum
 {
     // single lines
-    MC_TTY_FRM_VERT,
     MC_TTY_FRM_HORIZ,
+    MC_TTY_FRM_VERT,
     MC_TTY_FRM_LEFTTOP,
     MC_TTY_FRM_RIGHTTOP,
     MC_TTY_FRM_LEFTBOTTOM,
@@ -43,8 +81,8 @@ typedef enum
     MC_TTY_FRM_CROSS,
 
     // double lines
-    MC_TTY_FRM_DVERT,
     MC_TTY_FRM_DHORIZ,
+    MC_TTY_FRM_DVERT,
     MC_TTY_FRM_DLEFTTOP,
     MC_TTY_FRM_DRIGHTTOP,
     MC_TTY_FRM_DLEFTBOTTOM,
@@ -61,7 +99,9 @@ typedef enum
 
 /*** global variables defined in .c file *********************************************************/
 
-extern int mc_tty_frm[];
+// The actual characters used for frame drawing, as taken from the skin. Indexed by MC_TTY_FRM_*,
+// the values are either one of MCS_ACS_* or a character in the current locale.
+extern mc_tty_char_t mc_tty_frm[];
 
 extern char *tty_tgetstr (const char *name);
 
@@ -110,22 +150,22 @@ extern void tty_touch_screen (void);
 extern void tty_gotoyx (int y, int x);
 extern void tty_getyx (int *py, int *px);
 
-extern void tty_set_alt_charset (gboolean alt_charset);
-
 extern void tty_display_8bit (gboolean what);
-extern void tty_print_char (int c);
-extern void tty_print_alt_char (int c, gboolean single);
-extern void tty_print_anychar (int c);
+extern void tty_print_char (mc_tty_char_t c);
+extern void tty_print_anychar (mc_tty_char_t c);
 extern void tty_print_string (const char *s);
 extern void tty_printf (const char *s, ...) G_GNUC_PRINTF (1, 2);
 
 extern void tty_print_one_vline (gboolean single);
 extern void tty_print_one_hline (gboolean single);
-extern void tty_draw_hline (int y, int x, int ch, int len);
-extern void tty_draw_vline (int y, int x, int ch, int len);
+extern void tty_draw_hline (int y, int x, mc_tty_char_t ch, int len);
+extern void tty_draw_vline (int y, int x, mc_tty_char_t ch, int len);
 extern void tty_draw_box (int y, int x, int rows, int cols, gboolean single);
 extern void tty_draw_box_shadow (int y, int x, int rows, int cols, int shadow_color);
 extern void tty_fill_region (int y, int x, int rows, int cols, unsigned char ch);
+
+extern mc_tty_char_t tty_unicode_to_mc_acs (gunichar c);
+extern mc_tty_char_t tty_mc_acs_map (gunichar c);
 
 extern int tty_resize (int fd);
 extern void tty_refresh (void);
@@ -133,8 +173,6 @@ extern void tty_change_screen_size (void);
 
 /* Clear screen */
 extern void tty_clear_screen (void);
-
-extern int mc_tty_normalize_lines_char (const char *str);
 
 extern void tty_enter_ca_mode (void);
 extern void tty_exit_ca_mode (void);
