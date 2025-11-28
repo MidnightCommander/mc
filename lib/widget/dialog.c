MidnightCommander/mc

commit e99555c356a1ecf8e5d1f62e38435fbbf4ccc09c

Ticket #3160: Use new colors for listbox's selected entry
Introduce new colors dselnormal and dselfocus for unfocused and focused
listbox's selected entry, forked off from the here misused dfocus and
dhotfocus, respectively.

Signed-off-by: Egmont Koblinger <egmont@gmail.com>

lib/widget/dialog.c
@@ -456,18 +456,24 @@ dlg_set_default_colors (void)
     dialog_colors[DLG_COLOR_FOCUS] = COLOR_FOCUS;
     dialog_colors[DLG_COLOR_HOT_NORMAL] = COLOR_HOT_NORMAL;
     dialog_colors[DLG_COLOR_HOT_FOCUS] = COLOR_HOT_FOCUS;
+    dialog_colors[DLG_COLOR_SELECTED_NORMAL] = COLOR_SELECTED_NORMAL;
+    dialog_colors[DLG_COLOR_SELECTED_FOCUS] = COLOR_SELECTED_FOCUS;
     dialog_colors[DLG_COLOR_TITLE] = COLOR_TITLE;
 
     alarm_colors[DLG_COLOR_NORMAL] = ERROR_COLOR;
     alarm_colors[DLG_COLOR_FOCUS] = ERROR_FOCUS;
     alarm_colors[DLG_COLOR_HOT_NORMAL] = ERROR_HOT_NORMAL;
     alarm_colors[DLG_COLOR_HOT_FOCUS] = ERROR_HOT_FOCUS;
+    alarm_colors[DLG_COLOR_SELECTED_NORMAL] = ERROR_HOT_FOCUS;  // unused
+    alarm_colors[DLG_COLOR_SELECTED_FOCUS] = ERROR_FOCUS;       // unused
     alarm_colors[DLG_COLOR_TITLE] = ERROR_TITLE;
 
     listbox_colors[DLG_COLOR_NORMAL] = PMENU_ENTRY_COLOR;
     listbox_colors[DLG_COLOR_FOCUS] = PMENU_SELECTED_COLOR;
     listbox_colors[DLG_COLOR_HOT_NORMAL] = PMENU_ENTRY_COLOR;
     listbox_colors[DLG_COLOR_HOT_FOCUS] = PMENU_SELECTED_COLOR;
+    listbox_colors[DLG_COLOR_SELECTED_NORMAL] = PMENU_SELECTED_COLOR;  // unused
+    listbox_colors[DLG_COLOR_SELECTED_FOCUS] = PMENU_SELECTED_COLOR;   // unused
     listbox_colors[DLG_COLOR_TITLE] = PMENU_TITLE_COLOR;
 }
 

lib/widget/dialog.h
@@ -37,6 +37,8 @@ typedef enum
     DLG_COLOR_FOCUS,
     DLG_COLOR_HOT_NORMAL,
     DLG_COLOR_HOT_FOCUS,
+    DLG_COLOR_SELECTED_NORMAL,
+    DLG_COLOR_SELECTED_FOCUS,
     DLG_COLOR_TITLE,
     DLG_COLOR_COUNT
 } dlg_colors_enum_t;

lib/skin.h
@@ -25,90 +25,92 @@
 #define SHADOW_COLOR          mc_skin_color__cache[9]
 
 /* Dialog colors */
-#define COLOR_NORMAL     mc_skin_color__cache[10]
-#define COLOR_FOCUS      mc_skin_color__cache[11]
-#define COLOR_HOT_NORMAL mc_skin_color__cache[12]
-#define COLOR_HOT_FOCUS  mc_skin_color__cache[13]
-#define COLOR_TITLE      mc_skin_color__cache[14]
+#define COLOR_NORMAL          mc_skin_color__cache[10]
+#define COLOR_FOCUS           mc_skin_color__cache[11]
+#define COLOR_HOT_NORMAL      mc_skin_color__cache[12]
+#define COLOR_HOT_FOCUS       mc_skin_color__cache[13]
+#define COLOR_SELECTED_NORMAL mc_skin_color__cache[14]
+#define COLOR_SELECTED_FOCUS  mc_skin_color__cache[15]
+#define COLOR_TITLE           mc_skin_color__cache[16]
 
 /* Error dialog colors */
-#define ERROR_COLOR      mc_skin_color__cache[15]
-#define ERROR_FOCUS      mc_skin_color__cache[16]
-#define ERROR_HOT_NORMAL mc_skin_color__cache[17]
-#define ERROR_HOT_FOCUS  mc_skin_color__cache[18]
-#define ERROR_TITLE      mc_skin_color__cache[19]
+#define ERROR_COLOR      mc_skin_color__cache[17]
+#define ERROR_FOCUS      mc_skin_color__cache[18]
+#define ERROR_HOT_NORMAL mc_skin_color__cache[19]
+#define ERROR_HOT_FOCUS  mc_skin_color__cache[20]
+#define ERROR_TITLE      mc_skin_color__cache[21]
 
 /* Menu colors */
-#define MENU_ENTRY_COLOR    mc_skin_color__cache[20]
-#define MENU_SELECTED_COLOR mc_skin_color__cache[21]
-#define MENU_HOT_COLOR      mc_skin_color__cache[22]
-#define MENU_HOTSEL_COLOR   mc_skin_color__cache[23]
-#define MENU_INACTIVE_COLOR mc_skin_color__cache[24]
+#define MENU_ENTRY_COLOR    mc_skin_color__cache[22]
+#define MENU_SELECTED_COLOR mc_skin_color__cache[23]
+#define MENU_HOT_COLOR      mc_skin_color__cache[24]
+#define MENU_HOTSEL_COLOR   mc_skin_color__cache[25]
+#define MENU_INACTIVE_COLOR mc_skin_color__cache[26]
 
 /* Popup menu colors */
-#define PMENU_ENTRY_COLOR      mc_skin_color__cache[25]
-#define PMENU_SELECTED_COLOR   mc_skin_color__cache[26]
-#define PMENU_HOT_COLOR        mc_skin_color__cache[27]  // unused: not implemented yet
-#define PMENU_HOTSEL_COLOR     mc_skin_color__cache[28]  // unused: not implemented yet
-#define PMENU_TITLE_COLOR      mc_skin_color__cache[29]
+#define PMENU_ENTRY_COLOR      mc_skin_color__cache[27]
+#define PMENU_SELECTED_COLOR   mc_skin_color__cache[28]
+#define PMENU_HOT_COLOR        mc_skin_color__cache[29]  // unused: not implemented yet
+#define PMENU_HOTSEL_COLOR     mc_skin_color__cache[30]  // unused: not implemented yet
+#define PMENU_TITLE_COLOR      mc_skin_color__cache[31]
 
-#define BUTTONBAR_HOTKEY_COLOR mc_skin_color__cache[30]
-#define BUTTONBAR_BUTTON_COLOR mc_skin_color__cache[31]
+#define BUTTONBAR_HOTKEY_COLOR mc_skin_color__cache[32]
+#define BUTTONBAR_BUTTON_COLOR mc_skin_color__cache[33]
 
-#define STATUSBAR_COLOR        mc_skin_color__cache[32]
+#define STATUSBAR_COLOR        mc_skin_color__cache[34]
 
 /*
  * This should be selectable independently. Default has to be black background
  * foreground does not matter at all.
  */
-#define GAUGE_COLOR           mc_skin_color__cache[33]
-#define INPUT_COLOR           mc_skin_color__cache[34]
-#define INPUT_UNCHANGED_COLOR mc_skin_color__cache[35]
-#define INPUT_MARK_COLOR      mc_skin_color__cache[36]
-#define INPUT_HISTORY_COLOR   mc_skin_color__cache[37]
-#define COMMAND_HISTORY_COLOR mc_skin_color__cache[38]
+#define GAUGE_COLOR           mc_skin_color__cache[35]
+#define INPUT_COLOR           mc_skin_color__cache[36]
+#define INPUT_UNCHANGED_COLOR mc_skin_color__cache[37]
+#define INPUT_MARK_COLOR      mc_skin_color__cache[38]
+#define INPUT_HISTORY_COLOR   mc_skin_color__cache[39]
+#define COMMAND_HISTORY_COLOR mc_skin_color__cache[40]
 
-#define HELP_NORMAL_COLOR     mc_skin_color__cache[39]
-#define HELP_ITALIC_COLOR     mc_skin_color__cache[40]
-#define HELP_BOLD_COLOR       mc_skin_color__cache[41]
-#define HELP_LINK_COLOR       mc_skin_color__cache[42]
-#define HELP_SLINK_COLOR      mc_skin_color__cache[43]
-#define HELP_TITLE_COLOR      mc_skin_color__cache[44]
+#define HELP_NORMAL_COLOR     mc_skin_color__cache[41]
+#define HELP_ITALIC_COLOR     mc_skin_color__cache[42]
+#define HELP_BOLD_COLOR       mc_skin_color__cache[43]
+#define HELP_LINK_COLOR       mc_skin_color__cache[44]
+#define HELP_SLINK_COLOR      mc_skin_color__cache[45]
+#define HELP_TITLE_COLOR      mc_skin_color__cache[46]
 
-#define VIEW_NORMAL_COLOR     mc_skin_color__cache[45]
-#define VIEW_BOLD_COLOR       mc_skin_color__cache[46]
-#define VIEW_UNDERLINED_COLOR mc_skin_color__cache[47]
-#define VIEW_SELECTED_COLOR   mc_skin_color__cache[48]
+#define VIEW_NORMAL_COLOR     mc_skin_color__cache[47]
+#define VIEW_BOLD_COLOR       mc_skin_color__cache[48]
+#define VIEW_UNDERLINED_COLOR mc_skin_color__cache[49]
+#define VIEW_SELECTED_COLOR   mc_skin_color__cache[50]
 
 /*
  * editor colors - only 4 for normal, search->found, select, and whitespace
  * respectively
  * Last is defined to view color.
  */
-#define EDITOR_NORMAL_COLOR       mc_skin_color__cache[49]
-#define EDITOR_NONPRINTABLE_COLOR mc_skin_color__cache[50]
-#define EDITOR_BOLD_COLOR         mc_skin_color__cache[51]
-#define EDITOR_MARKED_COLOR       mc_skin_color__cache[52]
-#define EDITOR_WHITESPACE_COLOR   mc_skin_color__cache[53]
-#define EDITOR_RIGHT_MARGIN_COLOR mc_skin_color__cache[54]
-#define EDITOR_BACKGROUND         mc_skin_color__cache[55]
-#define EDITOR_FRAME              mc_skin_color__cache[56]
-#define EDITOR_FRAME_ACTIVE       mc_skin_color__cache[57]
-#define EDITOR_FRAME_DRAG         mc_skin_color__cache[58]
+#define EDITOR_NORMAL_COLOR       mc_skin_color__cache[51]
+#define EDITOR_NONPRINTABLE_COLOR mc_skin_color__cache[52]
+#define EDITOR_BOLD_COLOR         mc_skin_color__cache[53]
+#define EDITOR_MARKED_COLOR       mc_skin_color__cache[54]
+#define EDITOR_WHITESPACE_COLOR   mc_skin_color__cache[55]
+#define EDITOR_RIGHT_MARGIN_COLOR mc_skin_color__cache[56]
+#define EDITOR_BACKGROUND         mc_skin_color__cache[57]
+#define EDITOR_FRAME              mc_skin_color__cache[58]
+#define EDITOR_FRAME_ACTIVE       mc_skin_color__cache[59]
+#define EDITOR_FRAME_DRAG         mc_skin_color__cache[60]
 /* color of left 8 char status per line */
-#define LINE_STATE_COLOR      mc_skin_color__cache[59]
-#define BOOK_MARK_COLOR       mc_skin_color__cache[60]
-#define BOOK_MARK_FOUND_COLOR mc_skin_color__cache[61]
+#define LINE_STATE_COLOR      mc_skin_color__cache[61]
+#define BOOK_MARK_COLOR       mc_skin_color__cache[62]
+#define BOOK_MARK_FOUND_COLOR mc_skin_color__cache[63]
 
 /* Diff colors */
-#define DFF_ADD_COLOR             mc_skin_color__cache[62]
-#define DFF_CHG_COLOR             mc_skin_color__cache[63]
-#define DFF_CHH_COLOR             mc_skin_color__cache[64]
-#define DFF_CHD_COLOR             mc_skin_color__cache[65]
-#define DFF_DEL_COLOR             mc_skin_color__cache[66]
-#define DFF_ERROR_COLOR           mc_skin_color__cache[67]
+#define DFF_ADD_COLOR             mc_skin_color__cache[64]
+#define DFF_CHG_COLOR             mc_skin_color__cache[65]
+#define DFF_CHH_COLOR             mc_skin_color__cache[66]
+#define DFF_CHD_COLOR             mc_skin_color__cache[67]
+#define DFF_DEL_COLOR             mc_skin_color__cache[68]
+#define DFF_ERROR_COLOR           mc_skin_color__cache[69]
 
-#define MC_SKIN_COLOR_CACHE_COUNT 68
+#define MC_SKIN_COLOR_CACHE_COUNT 70
 
 /*** enums ***************************************************************************************/
 

lib/skin/colors.c
@@ -259,6 +259,8 @@ mc_skin_color_cache_init (void)
     COLOR_FOCUS = mc_skin_color_get ("dialog", "dfocus");
     COLOR_HOT_NORMAL = mc_skin_color_get ("dialog", "dhotnormal");
     COLOR_HOT_FOCUS = mc_skin_color_get ("dialog", "dhotfocus");
+    COLOR_SELECTED_NORMAL = mc_skin_color_get ("dialog", "dselnormal");
+    COLOR_SELECTED_FOCUS = mc_skin_color_get ("dialog", "dselfocus");
     COLOR_TITLE = mc_skin_color_get ("dialog", "dtitle");
 
     ERROR_COLOR = mc_skin_color_get ("error", "_default_");

lib/widget/listbox.c
@@ -148,7 +148,8 @@ listbox_draw (WListbox *l, gboolean focused)
 
     disabled = widget_get_state (wl, WST_DISABLED);
     normalc = disabled ? DISABLED_COLOR : colors[DLG_COLOR_NORMAL];
-    selc = disabled ? DISABLED_COLOR : colors[focused ? DLG_COLOR_HOT_FOCUS : DLG_COLOR_FOCUS];
+    selc = disabled ? DISABLED_COLOR
+                    : colors[focused ? DLG_COLOR_SELECTED_FOCUS : DLG_COLOR_SELECTED_NORMAL];
 
     if (l->list != NULL)
     {

misc/skins/dark.ini
@@ -46,6 +46,8 @@
     dfocus = blue;cyan
     dhotnormal = white;
     dhotfocus = white;cyan
+    dselnormal = blue;cyan
+    dselfocus = white;cyan
     dtitle = white;
 
 [error]

misc/skins/darkfar.ini
@@ -46,6 +46,8 @@
     dfocus = blue;cyan
     dhotnormal = white;
     dhotfocus = white;cyan
+    dselnormal = blue;cyan
+    dselfocus = white;cyan
     dtitle = white;
 
 [error]

misc/skins/default.ini
@@ -46,6 +46,8 @@
     dfocus = black;cyan
     dhotnormal = blue;lightgray
     dhotfocus = blue;cyan
+    dselnormal = black;cyan
+    dselfocus = blue;cyan
     dtitle = blue;lightgray
 
 [error]

commit 4948fe168b556f516710f2a718a9e4d5fbcb6eee

Ticket #4915: Fix the help window's title color
This broke in recent commit e99555c35.

Signed-off-by: Egmont Koblinger <egmont@gmail.com>

lib/widget/dialog.c
@@ -54,6 +54,7 @@
 dlg_colors_t dialog_colors;
 dlg_colors_t alarm_colors;
 dlg_colors_t listbox_colors;
+dlg_colors_t help_colors;
 
 /* A hook list for idle events */
 hook_t *idle_hook = NULL;
@@ -475,6 +476,14 @@ dlg_set_default_colors (void)
     listbox_colors[DLG_COLOR_SELECTED_NORMAL] = PMENU_SELECTED_COLOR;  // unused
     listbox_colors[DLG_COLOR_SELECTED_FOCUS] = PMENU_SELECTED_COLOR;   // unused
     listbox_colors[DLG_COLOR_TITLE] = PMENU_TITLE_COLOR;
+
+    help_colors[DLG_COLOR_NORMAL] = HELP_NORMAL_COLOR;
+    help_colors[DLG_COLOR_FOCUS] = 0;  // unused
+    help_colors[DLG_COLOR_HOT_NORMAL] = HELP_BOLD_COLOR;
+    help_colors[DLG_COLOR_HOT_FOCUS] = 0;        // unused
+    help_colors[DLG_COLOR_SELECTED_NORMAL] = 0;  // unused
+    help_colors[DLG_COLOR_SELECTED_FOCUS] = 0;   // unused
+    help_colors[DLG_COLOR_TITLE] = HELP_TITLE_COLOR;
 }
 
 /* --------------------------------------------------------------------------------------------- */

lib/widget/dialog.h
@@ -91,6 +91,7 @@ struct WDialog
 extern dlg_colors_t dialog_colors;
 extern dlg_colors_t alarm_colors;
 extern dlg_colors_t listbox_colors;
+extern dlg_colors_t help_colors;
 
 /* A hook list for idle events */
 extern hook_t *idle_hook;

src/help.c
@@ -1121,14 +1121,6 @@ gboolean
 help_interactive_display (const gchar *event_group_name, const gchar *event_name,
                           gpointer init_data, gpointer data)
 {
-    const dlg_colors_t help_colors = {
-        HELP_NORMAL_COLOR,  // common text color
-        0,                  // unused in help
-        HELP_BOLD_COLOR,    // bold text color
-        0,                  // unused in help
-        HELP_TITLE_COLOR    // title color
-    };
-
     Widget *wh;
     WGroup *g;
     WButtonBar *help_bar;