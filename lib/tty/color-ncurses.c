f50c88a580a6bdae606a5f24adc5335292261fb5
Egmont Koblinger <egmont@gmail.com>
2026-02-02 14:59:16 +0100

    Ticket #4985: Remove support for "special" monochrome colors
    These were only used internally by the hardcoded black and white skin,
    up until the previous commit.
    
    Signed-off-by: Egmont Koblinger <egmont@gmail.com>

--- lib/tty/color-ncurses.c
+++ lib/tty/color-ncurses.c
@@ -103,19 +103,6 @@ color_get_attr (int color_pair)
 }
 
 /* --------------------------------------------------------------------------------------------- */
-
-static void
-mc_tty_color_pair_init_special (tty_color_lib_pair_t *mc_color_pair, int fg1, int bg1, int fg2,
-                                int bg2, int attr)
-{
-    if (has_colors () && !mc_tty_color_disable)
-        init_pair (mc_color_pair->pair_index, fg1, bg1);
-    else
-        init_pair (mc_color_pair->pair_index, fg2, bg2);
-    mc_tty_color_save_attr (mc_color_pair->pair_index, attr);
-}
-
-/* --------------------------------------------------------------------------------------------- */
 /*** public functions ****************************************************************************/
 /* --------------------------------------------------------------------------------------------- */
 
@@ -153,78 +140,51 @@ tty_color_deinit_lib (void)
 void
 tty_color_try_alloc_lib_pair (tty_color_lib_pair_t *mc_color_pair)
 {
-    if (mc_color_pair->fg <= (int) SPEC_A_REVERSE)
+    int ifg, ibg, attr;
+
+    ifg = mc_color_pair->fg;
+    ibg = mc_color_pair->bg;
+    attr = mc_color_pair->attr;
+
+    // If we have 8 indexed colors only, change foreground bright colors into bold and
+    // background bright colors to basic colors
+    if (COLORS <= 8 || (tty_use_truecolors (NULL) && overlay_colors <= 8))
     {
-        switch (mc_color_pair->fg)
+        if (ifg >= 8 && ifg < 16)
         {
-        case SPEC_A_REVERSE:
-            mc_tty_color_pair_init_special (mc_color_pair, COLOR_BLACK, COLOR_WHITE, COLOR_BLACK,
-                                            COLOR_WHITE | A_BOLD, A_REVERSE);
-            break;
-        case SPEC_A_BOLD:
-            mc_tty_color_pair_init_special (mc_color_pair, COLOR_WHITE, COLOR_BLACK, COLOR_WHITE,
-                                            COLOR_BLACK, A_BOLD);
-            break;
-        case SPEC_A_BOLD_REVERSE:
-            mc_tty_color_pair_init_special (mc_color_pair, COLOR_WHITE, COLOR_WHITE, COLOR_WHITE,
-                                            COLOR_WHITE, A_BOLD | A_REVERSE);
-            break;
-        case SPEC_A_UNDERLINE:
-            mc_tty_color_pair_init_special (mc_color_pair, COLOR_WHITE, COLOR_BLACK, COLOR_WHITE,
-                                            COLOR_BLACK, A_UNDERLINE);
-            break;
-        default:
-            break;
+            ifg &= 0x07;
+            attr |= A_BOLD;
+        }
+
+        if (ibg >= 8 && ibg < 16)
+        {
+            ibg &= 0x07;
         }
     }
-    else
+
+    // Shady trick: if we don't have the exact color, because it is overlaid by backwards
+    // compatibility indexed values, just borrow one degree of red. The user won't notice :)
+    if ((ifg & FLAG_TRUECOLOR) != 0)
     {
-        int ifg, ibg, attr;
+        ifg &= ~FLAG_TRUECOLOR;
+        if (ifg != 0 && ifg <= overlay_colors)
+            ifg += (1 << 16);
+    }
 
-        ifg = mc_color_pair->fg;
-        ibg = mc_color_pair->bg;
-        attr = mc_color_pair->attr;
-
-        // If we have 8 indexed colors only, change foreground bright colors into bold and
-        // background bright colors to basic colors
-        if (COLORS <= 8 || (tty_use_truecolors (NULL) && overlay_colors <= 8))
-        {
-            if (ifg >= 8 && ifg < 16)
-            {
-                ifg &= 0x07;
-                attr |= A_BOLD;
-            }
-
-            if (ibg >= 8 && ibg < 16)
-            {
-                ibg &= 0x07;
-            }
-        }
-
-        // Shady trick: if we don't have the exact color, because it is overlaid by backwards
-        // compatibility indexed values, just borrow one degree of red. The user won't notice :)
-        if ((ifg & FLAG_TRUECOLOR) != 0)
-        {
-            ifg &= ~FLAG_TRUECOLOR;
-            if (ifg != 0 && ifg <= overlay_colors)
-                ifg += (1 << 16);
-        }
-
-        if ((ibg & FLAG_TRUECOLOR) != 0)
-        {
-            ibg &= ~FLAG_TRUECOLOR;
-            if (ibg != 0 && ibg <= overlay_colors)
-                ibg += (1 << 16);
-        }
+    if ((ibg & FLAG_TRUECOLOR) != 0)
+    {
+        ibg &= ~FLAG_TRUECOLOR;
+        if (ibg != 0 && ibg <= overlay_colors)
+            ibg += (1 << 16);
+    }
 
 #if NCURSES_VERSION_PATCH >= 20170401 && defined(NCURSES_EXT_COLORS) && defined(NCURSES_EXT_FUNCS) \
     && defined(HAVE_NCURSES_WIDECHAR)
-        init_extended_pair (mc_color_pair->pair_index, ifg, ibg);
+    init_extended_pair (mc_color_pair->pair_index, ifg, ibg);
 #else
-        init_pair (mc_color_pair->pair_index, ifg, ibg);
+    init_pair (mc_color_pair->pair_index, ifg, ibg);
 #endif
-        mc_tty_color_save_attr (mc_color_pair->pair_index, attr);
-    }
+    mc_tty_color_save_attr (mc_color_pair->pair_index, attr);
 }
 
 /* --------------------------------------------------------------------------------------------- */
