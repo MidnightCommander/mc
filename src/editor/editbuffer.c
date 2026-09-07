db6973d6be0c4f98a2c1325a0d8afd0729d3e1e5
Kuzin Andrey <kuzinandrey@yandex.ru>
2026-09-07 07:41:49 +0300

    editor(editbuffer): Add line break helpers to the text buffer
    Add three text buffer helpers that will be used to treat a Windows
    ("\r\n") line break as a single unit:
    
    - edit_buffer_is_crlf() checks whether the buffer contains a "\r\n"
      line break starting at the given position.
    - edit_buffer_detect_line_breaks() detects the line break type of the
      buffer content: LB_WIN if all line breaks are "\r\n", LB_UNIX if all
      are "\n", LB_MAC if all are "\r" and LB_ASIS for a mixture.
    - edit_buffer_trailing_ws_start() returns the start offset of the
      trailing whitespace (spaces and tabs) run of a line; for a CRLF line
      the line content ends at the "\r" of its line break, not at the "\n".
    
    Signed-off-by: Kuzin Andrey <kuzinandrey@yandex.ru>

--- src/editor/editbuffer.c
+++ src/editor/editbuffer.c
@@ -895,3 +895,90 @@ edit_buffer_calc_percent (const edit_buffer_t *buf, off_t offset)
 }
 
 /* --------------------------------------------------------------------------------------------- */
+/**
+ * Get the start offset of the trailing whitespace (spaces and tabs) run of the
+ * line that begins at the given offset. If the line has no trailing whitespace
+ * the end of the line content is returned, so no character of the line is at or
+ * after the result.
+ *
+ * @param buf editor buffer
+ * @param bol offset of the first character of the line
+ *
+ * @return start offset of the trailing whitespace run (or end of line content)
+ */
+
+off_t
+edit_buffer_trailing_ws_start (const edit_buffer_t *buf, off_t bol)
+{
+    off_t tws, eol;
+
+    eol = edit_buffer_get_eol (buf, bol);
+
+    // a CRLF line break's content ends at the "\r", not at the "\n"
+    if (eol > bol && edit_buffer_is_crlf (buf, eol - 1))
+        eol--;
+
+    for (tws = eol; tws > bol; tws--)
+    {
+        int c;
+
+        c = edit_buffer_get_byte (buf, tws - 1);
+        if (c != ' ' && c != '\t')
+            break;
+    }
+
+    return tws;
+}
+
+/* --------------------------------------------------------------------------------------------- */
+/**
+ * Detect the line break type used in the buffer content.
+ *
+ * @param buf editor buffer
+ *
+ * @return LB_WIN if all line breaks are "\r\n"; LB_UNIX if all line breaks
+ *         are "\n" (including buffers without any line breaks); LB_MAC if
+ *         all line breaks are "\r"; LB_ASIS if the buffer uses a mixture of
+ *         different line breaks (such a file can only be saved as-is)
+ */
+
+LineBreaks
+edit_buffer_detect_line_breaks (const edit_buffer_t *buf)
+{
+    off_t crlf = 0;  // "\r\n" line breaks
+    off_t cr = 0;    // "\r" line breaks not followed by "\n"
+    off_t lf = 0;    // "\n" line breaks not preceded by "\r"
+    off_t i;
+
+    for (i = 0; i < buf->size; i++)
+    {
+        if (edit_buffer_get_byte (buf, i) == '\r')
+        {
+            if (edit_buffer_is_crlf (buf, i))
+                crlf++;
+            else
+                cr++;
+        }
+        else if (edit_buffer_get_byte (buf, i) == '\n'
+                 && (i == 0 || edit_buffer_get_byte (buf, i - 1) != '\r'))
+            lf++;
+    }
+
+    if (crlf > 0)
+    {
+        if (cr > 0 || lf > 0)
+            return LB_ASIS;  // mixture of line breaks
+        return LB_WIN;
+    }
+
+    if (cr > 0)
+    {
+        if (lf > 0)
+            return LB_ASIS;  // mixture of line breaks
+        return LB_MAC;
+    }
+
+    return LB_UNIX;
+}
+
+/* --------------------------------------------------------------------------------------------- */
