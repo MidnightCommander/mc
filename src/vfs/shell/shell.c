b1abb0492dfcfbf6929522597d9d68259ce6dc2f
Phil Krylov <phil@krylov.eu>
2026-09-16 16:11:02 +0200

    vfs/shell: check if the path is writable in shell_open()
    Signed-off-by: Phil Krylov <phil@krylov.eu>

--- src/vfs/shell/shell.c
+++ src/vfs/shell/shell.c
@@ -1686,6 +1686,28 @@ shell_fill_names (struct vfs_class *me, fill_names_f func)
 static void *
 shell_open (const vfs_path_t *vpath, int flags, mode_t mode)
 {
+    if ((flags & O_WRONLY) != 0)
+    {
+        // We need to check if the path is writable to return the error ASAP
+        const char *name;
+        char *quoted_name;
+        char *command;
+        struct vfs_s_super *super;
+        struct vfs_class *me;
+        int r;
+
+        me = VFS_CLASS (vfs_path_get_last_path_vfs (vpath));
+        name = vfs_s_get_path (vpath, &super, 0);
+        quoted_name = str_shell_escape (name);
+        command = g_strdup_printf ("if : %s /%s; then echo '### 200'; else echo '### 500'; fi\n",
+                                   ((flags & O_APPEND) != 0) ? ">>" : ">", quoted_name);
+        g_free (quoted_name);
+        r = shell_command (me, super, WAIT_REPLY, command, -1);
+        g_free (command);
+        if (r != COMPLETE)
+            ERRNOR (E_REMOTE, NULL);
+    }
+
     /*
        sorry, i've places hack here
        cause shell don't able to open files with O_EXCL flag
